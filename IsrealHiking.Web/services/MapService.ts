3803329c37e289c0e8d8b5922c2da28a7b933b76
Harel Mazor <harel.mazor@gmail.com>
2015-11-01 23:33:35 +0200

    Add tests and code coverage #110 - Made chutzpah tests work finally!

--- IsrealHiking.Web/services/MapService.ts
+++ IsrealHiking.Web/services/MapService.ts
@@ -3,7 +3,7 @@
         public map: L.Map;
 
         constructor() {
-            this.map = L.map("map", <L.MapOptions>{
+            this.map = L.map("map", <L.Map.MapOptions>{
                 center: L.latLng(31.773, 35.12),
                 zoom: 13,
                 doubleClickZoom: false,
--- Tests/IsraelHiking.Web.Tests/services/MicrosoftElevationProviderTests.ts
+++ Tests/IsraelHiking.Web.Tests/services/MicrosoftElevationProviderTests.ts
@@ -9,60 +9,87 @@
 
 module IsraelHiking {
     describe("Microsoft Elevation Provider", () => {
-        var ADDRESS = "http://dev.virtualearth.net/REST/v1/Elevation/List?jsonp=JSON_CALLBACK&key=ArUJIOvdEI-4sFS5-3PqMlDJP-00FMLrOeLIGkLRpfWIjfpOcESgnE-Zmk-ZimU2&points=undefined,undefined";
+        var ADDRESS = "http://dev.virtualearth.net/REST/v1/Elevation/List?jsonp=JSON_CALLBACK&key=ArUJIOvdEI-4sFS5-3PqMlDJP-00FMLrOeLIGkLRpfWIjfpOcESgnE-Zmk-ZimU2&points=0.0000,0.0000";
         var elevationProvider: IsraelHiking.Services.Elevation.MicrosoftElevationProvider;
         var $http: angular.IHttpService;
         var $httpBackend: angular.IHttpBackendService;
         var toastr: Toastr;
 
         beforeEach(() => {
-            elevationProvider = new Services.Elevation.MicrosoftElevationProvider(null, null);
-            //angular.mock.inject((_$http_: angular.IHttpService, _$httpBackend_: angular.IHttpBackendService) => { // , _toastr_: Toastr
-            //    // The injector unwraps the underscores (_) from around the parameter names when matching
-            //    $http = _$http_;
-            //    $httpBackend = _$httpBackend_;
-            //    //toastr = _toastr_;
-            //    elevationProvider = new Services.Elevation.MicrosoftElevationProvider($http, null);
-            //});
+            angular.mock.module("toastr");
+            angular.mock.inject((_$http_: angular.IHttpService, _$httpBackend_: angular.IHttpBackendService, _toastr_: Toastr) => { // 
+                // The injector unwraps the underscores (_) from around the parameter names when matching
+                $http = _$http_;
+                $httpBackend = _$httpBackend_;
+                toastr = _toastr_;
+                elevationProvider = new Services.Elevation.MicrosoftElevationProvider($http, toastr);
+            });
         });
         
-        //it("Should update height data", () => {
-        //    var latlngzs = [<Common.LatLngZ>{ z: 0 }];
-        //    $httpBackend.when("JSONP", ADDRESS)
-        //        .respond({ resourceSets: [{ resources: [{ elevations: [1] }] }] });
-        //    elevationProvider.updateHeights(latlngzs);
-        //    $httpBackend.flush();
-        //    expect(latlngzs[0].z).toBe(1);
-        //});
-        //
-        //it("Should not update height data because no data returned", () => {
-        //    var latlngzs = [<Common.LatLngZ>{ z: 0 }];
-        //    $httpBackend.when("JSONP", ADDRESS)
-        //        .respond({ resourceSets: [{ resources: [{ elevations: [] }] }] });
-        //    elevationProvider.updateHeights(latlngzs);
-        //    $httpBackend.flush();
-        //    expect(latlngzs[0].z).toBe(0);
-        //});
-        //
-        //it("Should not update height data because no data has no sets", () => {
-        //    var latlngzs = [<Common.LatLngZ>{ z: 0 }];
-        //    $httpBackend.when("JSONP", ADDRESS)
-        //        .respond({ resourceSets: [] });
-        //    elevationProvider.updateHeights(latlngzs);
-        //    $httpBackend.flush();
-        //    expect(latlngzs[0].z).toBe(0);
-        //});
-        //
-        //it("Should not update height data because no data has no resources", () => {
-        //    var latlngzs = [<Common.LatLngZ>{ z: 0 }];
-        //    $httpBackend.when("JSONP", ADDRESS)
-        //        .respond({ resourceSets: [{ resources: [] }] });
-        //    elevationProvider.updateHeights(latlngzs);
-        //    $httpBackend.flush();
-        //    expect(latlngzs[0].z).toBe(0);
-        //});
-        it("Should run", () => {
-            expect(1).toBe(1);
+        it("Should update height data", () => {
+            var latlngzs = [<Common.LatLngZ>{lat: 0, lng: 0, z: 0 }];
+            $httpBackend.when("JSONP", ADDRESS)
+                .respond({ resourceSets: [{ resources: [{ elevations: [1] }] }] });
+        
+            elevationProvider.updateHeights(latlngzs);
+            $httpBackend.flush();
+        
+            expect(latlngzs[0].z).toBe(1);
+        });
+        
+        it("Should not update height data because no data returned", () => {
+            var latlngzs = [<Common.LatLngZ>{ lat: 0, lng: 0, z: 0 }];
+            $httpBackend.when("JSONP", ADDRESS)
+                .respond({ resourceSets: [{ resources: [{ elevations: [] }] }] });
+        
+            elevationProvider.updateHeights(latlngzs);
+            $httpBackend.flush();
+        
+            expect(latlngzs[0].z).toBe(0);
+        });
+        
+        it("Should not update height data because data has no sets", () => {
+            var latlngzs = [<Common.LatLngZ>{ lat: 0, lng: 0, z: 0 }];
+            $httpBackend.when("JSONP", ADDRESS)
+                .respond({ resourceSets: [] });
+        
+            elevationProvider.updateHeights(latlngzs);
+            $httpBackend.flush();
+        
+            expect(latlngzs[0].z).toBe(0);
+        });
+        
+        it("Should not update height data because data has no resources", () => {
+            var latlngzs = [<Common.LatLngZ>{ lat: 0, lng: 0, z: 0 }];
+            $httpBackend.when("JSONP", ADDRESS)
+                .respond({ resourceSets: [{ resources: [] }] });
+        
+            elevationProvider.updateHeights(latlngzs);
+            $httpBackend.flush();
+        
+            expect(latlngzs[0].z).toBe(0);
+        });
+        
+        it("Should not call provider bacause all coordinates has elevation", () => {
+            var latlngzs = [<Common.LatLngZ>{ lat: 0, lng: 0, z: 1 }];
+        
+            elevationProvider.updateHeights(latlngzs);
+        
+            expect(latlngzs[0].z).toBe(1);
+            $httpBackend.verifyNoOutstandingExpectation();
+        });
+
+        it("Should raise toast when error occurs", () => {
+            var latlngzs = [<Common.LatLngZ>{ lat: 0, lng: 0, z: 0 }];
+            $httpBackend.when("JSONP", ADDRESS)
+                .respond(500, {});
+            toastr.error = jasmine.createSpy("spy");
+
+            elevationProvider.updateHeights(latlngzs);
+            $httpBackend.flush();
+
+            expect(toastr.error).toHaveBeenCalled();
+            
         });
     });
 }
