669983e47a488db42c04072911b1e7815ec9c6ed
Harel Mazor <harel.mazor@gmail.com>
2016-08-21 00:23:38 +0300

    Added google analytics to oruxmaps download page.

--- IsrealHiking.Web/Oruxmaps.ts
+++ IsrealHiking.Web/Oruxmaps.ts
@@ -1,10 +1,14 @@
 ﻿namespace IsraelHiking {
     export var app = angular.module("Oruxmaps", []);
 
-    app.controller("OruxmapsController", ["$scope", "$window", ($scope, $window: angular.IWindowService) => new OruxmapsController($scope, $window)]);
+    app.controller("OruxmapsController", ["$scope", "$window", "$timeout",
+        ($scope, $window: angular.IWindowService, $timeout: angular.ITimeoutService) =>
+            new OruxmapsController($scope, $window, $timeout)]);
 
     export class OruxmapsController {
-        constructor($scope, $window: angular.IWindowService) {
+        constructor($scope,
+            $window: angular.IWindowService,
+            $timeout: angular.ITimeoutService) {
             $scope.links = [
                 {
                     url: "orux-map://israelhiking.osm.org.il/Oruxmaps/IsraelHiking.zip",
@@ -23,10 +27,13 @@
                 $window.location.href = address;
             }
 
-            if ($window.location.hash) {
+            if ($window.location.hash && navigator.userAgent.match(/Android/i)) {
                 for (let link of $scope.links) {
                     if ($window.location.hash.toLowerCase().indexOf(link.hash.toLowerCase()) !== -1) {
-                        $scope.redirect(link.url);
+                        $timeout(() => {
+                            $window.location.href = link.url;
+                        }, 500);
+                        return;
                     }
                 }    
             }
