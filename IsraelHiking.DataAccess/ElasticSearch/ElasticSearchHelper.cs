d1efb2db639f1d8988da7d0efc2816d7c3f1e235
Harel Mazor <harel.mazor@gmail.com>
2016-08-25 18:52:07 +0300

    Updated elastic search references.

--- IsraelHiking.DataAccess/ElasticSearch/ElasticSearchHelper.cs
+++ IsraelHiking.DataAccess/ElasticSearch/ElasticSearchHelper.cs
@@ -7,7 +7,7 @@ namespace IsraelHiking.DataAccess.ElasticSearch
         protected override string RelativePath => "ElasticSearch";
         protected override string Name => "\"Elastic Search Service\"";
         protected override string Description => "Name search service for israel hiking site";
-        protected override string CommandLine => "java -Delasticsearch -Des-foreground=yes -Des.path.home=\"./ \" -cp \"lib/elasticsearch-2.2.1.jar;lib/*\" \"org.elasticsearch.bootstrap.Elasticsearch\" start";
+        protected override string CommandLine => "java -Delasticsearch -Des-foreground=yes -Des.path.home=\"./ \" -cp \"lib/elasticsearch-2.3.5.jar;lib/*\" \"org.elasticsearch.bootstrap.Elasticsearch\" start";
 
         public ElasticSearchHelper(ILogger logger, IProcessHelper processHelper) : base(logger, processHelper)
         {
