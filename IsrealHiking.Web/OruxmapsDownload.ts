9625d3a8d71155bdeed281349625c9122dc70503
Harel Mazor <harel.mazor@gmail.com>
2016-08-25 18:23:00 +0300

    Updated oruxmaps download page #204

--- IsrealHiking.Web/OruxmapsDownload.ts
+++ IsrealHiking.Web/OruxmapsDownload.ts
@@ -1,30 +1,67 @@
-﻿namespace IsraelHiking {
+﻿namespace IsraelHiking.Oruxmaps {
+
+    export interface ILink {
+        url: string;
+        description: string;
+        descriptionHebrew: string;
+        hash: string;
+    }
+
+    export interface IOruxmpasScope extends angular.IScope {
+        links: ILink[];
+        isHebrew: boolean;
+        redirect(address: string): void;
+    }
+
     export var app = angular.module("Oruxmaps", []);
+
+    app.config($compileProvider => {
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|orux-map):/);
+    });
 
     app.controller("OruxmapsController", ["$scope", "$window", "$timeout",
         ($scope, $window: angular.IWindowService, $timeout: angular.ITimeoutService) =>
             new OruxmapsController($scope, $window, $timeout)]);
 
     export class OruxmapsController {
-        constructor($scope,
+        constructor($scope: IOruxmpasScope,
             $window: angular.IWindowService,
             $timeout: angular.ITimeoutService) {
+
+            $scope.isHebrew = true;
             $scope.links = [
                 {
-                    url: "orux-map://israelhiking.osm.org.il/Oruxmaps/IsraelHiking.zip",
-                    description: "Israel Hiking Map zoom 15",
-                    descriptionHebrew: "מפת הטיולים החופשית זום 15",
+                    url: "//israelhiking.osm.org.il/Oruxmaps/IsraelHiking.zip",
+                    description: "Israel Hiking Map up to zoom 15 [750MB]",
+                    descriptionHebrew: "מפת הטיולים הפתוחה עד זום 15 [750MB]",
                     hash: "IHM15"
                 },
                 {
-                    url: "orux-map://israelhiking.osm.org.il/Oruxmaps/IsraelMTB.zip",
-                    description: "Israel MTB Map zoom 15",
-                    descriptionHebrew: "מפת האופניים החופשית זום 15",
+                    url: "//israelhiking.osm.org.il/Oruxmaps/IsraelMTB.zip",
+                    description: "Israel MTB Map up to zoom 15 [720MB]",
+                    descriptionHebrew: "מפת האופניים הפתוחה עד זום 15 [720MB]",
                     hash: "IMTBM15"
+                },
+                {
+                    url: "//israelhiking.osm.org.il/Oruxmaps/IsraelHiking16.zip",
+                    description: "Israel Hiking Map up to zoom 16 [2.5GB]",
+                    descriptionHebrew: "מפת הטיולים הפתוחה עד זום 16 [2.5GB]",
+                    hash: "IHM16"
+                },
+                {
+                    url: "//israelhiking.osm.org.il/Oruxmaps/IsraelMTB16.zip",
+                    description: "Israel MTB Map up to zoom 16 [2.2GB]",
+                    descriptionHebrew: "מפת האופניים הפתוחה עד זום 16 [2.2GB]",
+                    hash: "IMTBM16"
                 }
             ];
+
             $scope.redirect = (address: string) => {
-                $window.location.href = address;
+                if (navigator.userAgent.match(/Android/i)) {
+                    return `orux-map:${address}`;
+                } else {
+                    return address;
+                }
             }
 
             if ($window.location.hash && navigator.userAgent.match(/Android/i)) {
@@ -35,7 +72,7 @@
                         }, 500);
                         return;
                     }
-                }    
+                }
             }
         }
     }
