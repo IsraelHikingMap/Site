3d4a413606aa5e7b774eb8d50e1171f9d3fb9c66
Harel Mazor <harel.mazor@gmail.com>
2017-06-21 00:45:30 +0300

    Upgrade angular to 4.0.x #349 - small fix in osm user html, update build script.

--- AppVeyorBuild/BuildTypescript.ps1
+++ AppVeyorBuild/BuildTypescript.ps1
@@ -9,15 +9,13 @@ Set-Location -Path $env:APPVEYOR_BUILD_FOLDER
 
 # Restore all packages
 
+Write-Host "dotnet restore - to restore NuGet packages for build"
+
 dotnet restore
 
 # Locate executables
 
-$npm = get-childitem "C:\Program Files (x86)\Microsoft Visual Studio\2017" npm.cmd -recurse | select-object -first 1 | select -expand FullName
-
-$tsc = get-childitem "C:\Users\$($env:UserName)\.nuget\packages\" tsc.exe -recurse | select-object -first 1 | select -expand FullName
-
-$webProjects = @("IsraelHiking.Web", "Tests\IsraelHiking.Web.Tests")
+$webProjects = @("IsraelHiking.Web") #, "Tests\IsraelHiking.Web.Tests")
 
 # Compile TypeScript files
 
@@ -26,13 +24,11 @@ foreach ($project in $webProjects)
 	$path = Join-Path $env:APPVEYOR_BUILD_FOLDER $project
 
 	Set-Location -Path $path
-    Write-Host "$($npm) install - to restore npm packages for typescript build" 
-	& $npm install
+    Write-Host "npm install - to restore npm packages for typescript build" 
+	npm install
 
-	Set-Location -Path $env:APPVEYOR_BUILD_FOLDER
-
-	Write-Host $tsc "-p $($project)"
-	& $tsc -p $project
+	Write-Host "ng build - to build using angular-cli"
+	ng build
 	
 	if($LastExitCode -ne 0) 
 	{ 
