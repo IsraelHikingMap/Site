a76f1b0e331884643a271005f9586515c708b0f4
Harel Mazor <harel.mazor@gmail.com>
2017-07-21 15:29:25 +0300

    Added xml comments in order to remove warnings.

--- IsraelHiking.API/Converters/ConverterFlows/IConverterFlowItem.cs
+++ IsraelHiking.API/Converters/ConverterFlows/IConverterFlowItem.cs
@@ -5,19 +5,61 @@
     /// </summary>
     public static class FlowFormats
     {
+        /// <summary>
+        /// GeoJson
+        /// </summary>
         public const string GEOJSON = "geojson";
+        /// <summary>
+        /// GPX
+        /// </summary>
         public const string GPX = "gpx";
+        /// <summary>
+        /// KML
+        /// </summary>
         public const string KML = "kml";
+        /// <summary>
+        /// Navigude - TWL
+        /// </summary>
         public const string TWL = "twl";
+        /// <summary>
+        /// KMZ - Zipped KML
+        /// </summary>
         public const string KMZ = "kmz";
+        /// <summary>
+        /// GPX 1.1 alias in GPSBabel
+        /// </summary>
         public const string GPX_BABEL_FORMAT = "gpx,gpxver=1.1";
+        /// <summary>
+        /// GPX 1.0 alias in GPSBabel
+        /// </summary>
         public const string GPX_BABEL_FORMAT_VERSION_1 = "gpx,gpxver=1.0";
+        /// <summary>
+        /// KML alias in GPSBabel
+        /// </summary>
         public const string KML_BABEL_FORMAT = "kml,points=0";
+        /// <summary>
+        /// Naviguide alias in GPSBabel
+        /// </summary>
         public const string TWL_BABEL_FORMAT = "naviguide";
+        /// <summary>
+        /// CSV alias in GPSBabel
+        /// </summary>
         public const string CSV_BABEL_FORMAT = "csv";
+        /// <summary>
+        /// Internal format for merging all routes in a single GPX track
+        /// </summary>
         public const string GPX_SINGLE_TRACK = "gpx_single_track";
+        /// <summary>
+        /// Internal format for merging all routes in a single GPX route
+        /// </summary>
         public const string GPX_ROUTE = "gpx_route";
+        /// <summary>
+        /// G-Zipped GPX
+        /// </summary>
         public const string GPX_GZ = "gpx.gz";
+        /// <summary>
+        /// BZ2 compressed GPX
+        /// </summary>
         public const string GPX_BZ2 = "gpx.bz2";
     }
 
