63d2fe69601e6b9644d1d1aed1c1cbbb8d9e28e1
Harel Mazor <harel.mazor@gmail.com>
2017-08-04 10:29:21 +0300

    #110 - added tests to handlers

---
+++ IsraelHiking.Web/sources/application/services/layers/routelayers/hover-handler.spec.ts
@@ -0,0 +1,59 @@
+﻿import { TestBed } from "@angular/core/testing";
+
+import { HoverHandler } from "./hover-handler";
+import {MapServiceMockCreator} from "../../../services/map.service.spec";
+
+describe("HoverHandler", () => {
+    var context;
+    var hoverHandler: HoverHandler;
+    var mapServiceMockCreator: MapServiceMockCreator;
+    var middleMarker: L.Marker;
+    
+    beforeEach(() => {
+        mapServiceMockCreator = new MapServiceMockCreator();
+        middleMarker = L.marker([0, 0]);
+        context = {
+            route: {
+                properties: {
+                    pathOptions: {
+                        opacity: 1
+                    }
+                }
+            },
+            mapService: mapServiceMockCreator.mapService
+        };
+        hoverHandler = new HoverHandler(context, middleMarker);
+    });
+
+    afterEach(() => {
+        mapServiceMockCreator.destructor();
+    });
+    
+    it("Should initialize with None state", () => {
+        expect(hoverHandler.getState()).toBe(HoverHandler.NONE);
+    });
+
+    it("Should be hidden when on marker", () => {
+        hoverHandler.setState(HoverHandler.ON_MARKER);
+
+        let layersNumber = 0;
+        context.mapService.map.eachLayer(() => layersNumber++);
+        expect(layersNumber).toBe(0);
+    });
+
+    it("Should be show middle marker when on polyline", () => {
+        hoverHandler.setState(HoverHandler.ON_POLYLINE);
+
+        let layersNumber = 0;
+        context.mapService.map.eachLayer(() => layersNumber++);
+        expect(layersNumber).toBe(1);
+    });
+
+    it("Should be show show line and marker when in add point state", () => {
+        hoverHandler.setState(HoverHandler.ADD_POINT);
+
+        let layersNumber = 0;
+        context.mapService.map.eachLayer(() => layersNumber++);
+        expect(layersNumber).toBe(3); // including svg layer for marker icon
+    });
+});
---
+++ IsraelHiking.Web/sources/application/services/layers/routelayers/undo-handler.spec.ts
@@ -0,0 +1,46 @@
+﻿import { UndoHandler } from "./undo-handler";
+
+describe("UndoHandler", () => {
+    it("Should be created disable", () => {
+        let undoHandler = new UndoHandler<number>();
+
+        expect(undoHandler.isUndoDisbaled).toBeTruthy();
+    });
+
+    it("Should be created disable", () => {
+        let undoHandler = new UndoHandler<number>();
+
+        expect(undoHandler.isUndoDisbaled()).toBeTruthy();
+    });
+
+    it("Should facilitate adding data", () => {
+        let undoHandler = new UndoHandler<number>();
+
+        undoHandler.addDataToUndoStack(42);
+        undoHandler.addDataToUndoStack(7);
+        
+        expect(undoHandler.isUndoDisbaled()).toBeFalsy();
+        expect(undoHandler.top()).toBe(7);
+    });
+
+    it("Should facilitate removing data", () => {
+        let undoHandler = new UndoHandler<number>();
+
+        undoHandler.addDataToUndoStack(42);
+        undoHandler.addDataToUndoStack(7);
+        undoHandler.pop();
+
+        expect(undoHandler.top()).toBe(42);
+    });
+
+    it("Should keep the last item in the undo stack", () => {
+        let undoHandler = new UndoHandler<number>();
+
+        undoHandler.addDataToUndoStack(42);
+        undoHandler.pop();
+        undoHandler.pop();
+        undoHandler.pop();
+
+        expect(undoHandler.top()).toBe(42);
+    });
+});
