91890bf7db67ac2b6b0729c99c3d5920108c9e5c
Harel Mazor <harel.mazor@gmail.com>
2017-09-12 22:45:34 +0300

    Check Graphhopper memory usage #382 - will hopefuly do the trick.

--- IsraelHiking.DataAccess/GraphHopper/GraphHopper.cmd
+++ IsraelHiking.DataAccess/GraphHopper/GraphHopper.cmd
@@ -1,4 +1,4 @@
 @REM starts the graphhopper server
 PUSHD %~dp0
-java -cp "*;web\*" com.graphhopper.http.GHServer config=israel-hiking-config.properties
+java -cp "*;web\*" com.graphhopper.http.GHServer config=israel-hiking-config.properties -Xmx2048m
 POPD
