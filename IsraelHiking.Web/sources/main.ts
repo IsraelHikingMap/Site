3165a2b22338ff4f56c7cadc394f0b0b757fcc25
Harel Mazor <harel.mazor@gmail.com>
2017-09-15 13:33:24 +0300

    Site get stuck in loading in safari desktop (in iphone it's working) #460 - Added more polyfills, remove all warnings.

--- IsraelHiking.Web/sources/main.ts
+++ IsraelHiking.Web/sources/main.ts
@@ -2,6 +2,7 @@ import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { ApplicationModule } from "./application/application.module";
 import { environment } from "./environments/environment";
+import "hammerjs";
 
 if (environment.production) {
   enableProdMode();
