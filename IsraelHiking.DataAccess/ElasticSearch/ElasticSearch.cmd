d415364e6c66277bd331fa1eb52bf4215a1dd1c9
zstadler <zeev.stadler@gmail.com>
2017-09-22 18:44:06 +0300

    Memory setting for Java processes
    Apply same settiing to GraphHopper and ElasticSearch.
    Resolve #382

--- IsraelHiking.DataAccess/ElasticSearch/ElasticSearch.cmd
+++ IsraelHiking.DataAccess/ElasticSearch/ElasticSearch.cmd
@@ -1,4 +1,7 @@
 @REM starts the elastic search server
 PUSHD %~dp0
-java -Delasticsearch -Des-foreground=yes -Des.path.home="./" -cp "lib/elasticsearch-2.3.5.jar;lib/*" "org.elasticsearch.bootstrap.Elasticsearch" start
+@REM For GC debug with GCViewer https://github.com/chewiebug/gcviewer/wiki
+@REM SET GC_DEBUG=-Xloggc:elasticsearch.loggc -XX:+PrintGCDetails -XX:+PrintGCDateStamps
+
+java -Xmx1024m %GC_DEBUG% -Delasticsearch -Des-foreground=yes -Des.path.home="./" -cp "lib/elasticsearch-2.3.5.jar;lib/*" "org.elasticsearch.bootstrap.Elasticsearch" start
 POPD
--- IsraelHiking.DataAccess/GraphHopper/GraphHopper.cmd
+++ IsraelHiking.DataAccess/GraphHopper/GraphHopper.cmd
@@ -1,4 +1,7 @@
 @REM starts the graphhopper server
 PUSHD %~dp0
-java -cp "*;web\*" com.graphhopper.http.GHServer config=israel-hiking-config.properties -Xmx2048m
+@REM For GC debug with GCViewer https://github.com/chewiebug/gcviewer/wiki
+@REM SET GC_DEBUG=-Xloggc:graphhopper.loggc -XX:+PrintGCDetails -XX:+PrintGCDateStamps
+
+java -Xmx2048m %GC_DEBUG% -cp "*;web\*" com.graphhopper.http.GHServer config=israel-hiking-config.properties
 POPD
