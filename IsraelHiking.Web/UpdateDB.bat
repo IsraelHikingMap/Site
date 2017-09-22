0c880d03f66d42f0bea0c022bc82481fce17716b
Harel Mazor <harel.mazor@gmail.com>
2017-09-22 10:27:09 +0300

    Fixes #480 - Update database does not run on server.

--- IsraelHiking.Web/UpdateDB.bat
+++ IsraelHiking.Web/UpdateDB.bat
@@ -1,4 +1,4 @@
 @REM Updates the site's OSM database uing http post request.
 @REM Usage UpdateDB <updated osm.pbf file>
 
-curl -F "file=@%1;filename=israel-and-palestine-latest.osm.pbf" http://israelhiking.osm.org.il/api/update/
+curl -k -F "file=@%1;filename=israel-and-palestine-latest.osm.pbf" https://israelhiking.osm.org.il/api/update/
