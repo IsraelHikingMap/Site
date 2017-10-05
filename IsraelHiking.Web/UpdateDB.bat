a38326caaafdb2b55c633a20ee4a2697376d8413
Ofer Zelig <ofer.zelig@gmail.com>
2017-10-05 23:02:54 +0300

    Typo corrections

--- IsraelHiking.Web/UpdateDB.bat
+++ IsraelHiking.Web/UpdateDB.bat
@@ -1,4 +1,4 @@
-@REM Updates the site's OSM database uing http post request.
+@REM Updates the site's OSM database using http post request.
 @REM Usage UpdateDB <updated osm.pbf file>
 
 curl -k -F "file=@%1;filename=israel-and-palestine-latest.osm.pbf" https://israelhiking.osm.org.il/api/update/
