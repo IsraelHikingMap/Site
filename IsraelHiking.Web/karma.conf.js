383b0b8830fd712d0c935bf90e6161cd3868f8f5
Harel Mazor <harel.mazor@gmail.com>
2017-10-11 00:11:58 +0300

    Moved from coveralls to codecov, added yaml for build.

--- IsraelHiking.Web/karma.conf.js
+++ IsraelHiking.Web/karma.conf.js
@@ -10,20 +10,19 @@ module.exports = function (config) {
             require("karma-chrome-launcher"),
             require("karma-jasmine-html-reporter"),
             require("karma-coverage-istanbul-reporter"),
-            require("karma-junit-reporter"),
-            require("@angular/cli/plugins/karma"),
+            require("@angular/cli/plugins/karma")
         ],
         client: {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
         coverageIstanbulReporter: {
-            reports: ["html", "lcovonly"],
-            fixWebpackSourcePaths: true,
+            reports: ["lcovonly"],
+            fixWebpackSourcePaths: true
         },
         angularCli: {
             environment: "dev"
         },
-        reporters: ["progress", "kjhtml", "junit", "coverage-istanbul"],
+        reporters: ["progress", "coverage-istanbul"],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
