c4a17f003a98bb5d2df8b718907163606cf8eced
Harel Mazor <harel.mazor@gmail.com>
2017-10-22 23:51:08 +0300

    Added tests to conterollers.

--- Tests/IsraelHiking.API.Tests/Controllers/FilesControllerTests.cs
+++ Tests/IsraelHiking.API.Tests/Controllers/FilesControllerTests.cs
@@ -64,6 +64,14 @@ namespace IsraelHiking.API.Tests.Controllers
         }
 
         [TestMethod]
+        public void GetSupportedFileTypes_ShouldGetThem()
+        {
+            var results = _controller.GetSupportedFileTypes();
+
+            Assert.IsTrue(results.Count > 0);
+        }
+
+        [TestMethod]
         public void GetRemoteFile_ConvertKmlToGeoJson_ShouldReturnOnePointAndOneLineString()
         {
             var url = "someurl";
