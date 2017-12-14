7054d46c4a9e1ae25183fb2e80ceec29dfb5ab30
zstadler <zeev.stadler@gmail.com>
2017-12-14 20:19:52 +0200

    Reduce disk usage for server's log files
    Resolve  #577

--- Scripts/CleanLogs.cmd
+++ Scripts/CleanLogs.cmd
@@ -1,1 +1,2 @@
 forfiles -p "C:\inetpub\logs\LogFiles\W3SVC3" -s -m *.* -d -60 -c "cmd /c del @path"
+forfiles -p "C:\inetpub\logs\LogFiles\W3SVC3" -s -m *.log -d -3 -c "cmd /c 7z.exe a @path.zip @path && del @path"
