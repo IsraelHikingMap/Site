badd963b61328f7223c0c67b1171f47602c6228f
zstadler <zeev.stadler@gmail.com>
2018-01-26 12:53:35 +0200

    Additional minutes and seconds Unicode symbols (#627)
    Resolve Resolve #626

--- IsraelHiking.API/Converters/CoordinatesParsers/DegreesMinutesSecondsLatLonParser.cs
+++ IsraelHiking.API/Converters/CoordinatesParsers/DegreesMinutesSecondsLatLonParser.cs
@@ -9,17 +9,25 @@ namespace IsraelHiking.API.Converters.CoordinatesParsers
     public class DegreesMinutesSecondsLatLonParser : BaseCoordinatesParser
     {
         /// <summary>
+        /// Minutes symbol characters string (not a regex)
+        /// </summary>
+        public const string MINUTES_SYMBOLS_STRING = @"\u0027\u00b4\u02b9\u02bc\u02ca\u201d\u2032\u275c";
+        /// <summary>
+        /// Seconds symbol characters string (not a regex)
+        /// </summary>
+        public const string SECONDS_SYMBOL_STRING = @"\u0022\u02ba\u201d\u2033\u275e\u3003\u301e";
+        /// <summary>
         /// Degrees regex string
         /// </summary>
-        public const string DEGREES_REGEX_STRING = @"([\d\.°':""\u2032\u2033\s]+)";
+        public const string ANY_DEGREES_REGEX_STRING = @"([\d\.°" + MINUTES_SYMBOLS_STRING + SECONDS_SYMBOL_STRING + @":\s]+)";
         /// <summary>
         /// Degrees regex string with north-south postfix
         /// </summary>
-        public const string NORTH_SOUTH_REGEX_STRING = DEGREES_REGEX_STRING + "([NS])";
+        public const string NORTH_SOUTH_REGEX_STRING = ANY_DEGREES_REGEX_STRING + "([NS])";
         /// <summary>
         /// Degrees regex string with east-west postfix
         /// </summary>
-        public const string EAST_WEST_REGEX_STRING = DEGREES_REGEX_STRING + "([EW])";
+        public const string EAST_WEST_REGEX_STRING = ANY_DEGREES_REGEX_STRING + "([EW])";
 
         private readonly Regex _degreesMinutesSecondsRegex;
         private readonly Regex _decimalDegreesRegex;
@@ -34,8 +42,13 @@ namespace IsraelHiking.API.Converters.CoordinatesParsers
         public DegreesMinutesSecondsLatLonParser()
         {
             _decimalDegreesRegex = new Regex("^" + DecimalLatLonParser.DECIMAL_DEGREES_REGEX_STRING + "$");
-            _degreesMinutesSecondsRegex = new Regex(@"^\s*(\d{1,3})(?:[:°\s]\s*)(\d{1,2})(?:[:'\u2032\s]\s*)(\d{1,2}(?:\.\d+)?)[:""\u2033]?\s*$");
-            _degreesMinutesRegex = new Regex(@"^\s*(\d{1,3})(?:[:°\s]\s*)(\d{1,2}(?:\.\d+)?)[:'\u2032']?\s*$");
+            _degreesMinutesSecondsRegex = 
+                new Regex(@"^\s*(\d{1,3})(?:[:°\s]\s*)(\d{1,2})(?:[:" + 
+                          MINUTES_SYMBOLS_STRING + @"\s]\s*)(\d{1,2}(?:\.\d+)?)[:" +
+                          SECONDS_SYMBOL_STRING + @"]?\s*$");
+            _degreesMinutesRegex = 
+                new Regex(@"^\s*(\d{1,3})(?:[:°\s]\s*)(\d{1,2}(?:\.\d+)?)[:" +
+                          MINUTES_SYMBOLS_STRING + @"]?\s*$");
         }
 
         /// <inheritdoc/>
