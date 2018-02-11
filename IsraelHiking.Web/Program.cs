289aa1ac46cbba59e0916ffc379d7153499986f8
Nataly Shrits <nataly87s@gmail.com>
2018-02-11 21:44:05 +0200

    add docker support

--- IsraelHiking.Web/Program.cs
+++ IsraelHiking.Web/Program.cs
@@ -13,6 +13,7 @@ namespace IsraelHiking.Web
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:5000")
                 .Build()
                 .Run();
 
