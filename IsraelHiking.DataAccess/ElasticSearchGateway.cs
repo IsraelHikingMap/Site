d13f820df47ea35123c2a80c525de17a80aacb11
Harel Mazor <harel.mazor@gmail.com>
2018-07-25 00:50:33 +0300

    Improve loading time of shares with images #765 - fix pagination.

--- IsraelHiking.DataAccess/ElasticSearchGateway.cs
+++ IsraelHiking.DataAccess/ElasticSearchGateway.cs
@@ -471,7 +471,7 @@ namespace IsraelHiking.DataAccess
 
         public async Task<List<ShareUrl>> GetUrls(int page)
         {
-            var response = await _elasticClient.SearchAsync<ShareUrl>(s => s.Index(SHARES).Size(1000).Skip(1000 * page));
+            var response = await _elasticClient.SearchAsync<ShareUrl>(s => s.Index(SHARES).Size(1000).From(1000 * page));
             return response.Documents.ToList();
         }
 
