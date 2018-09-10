4e06c01e20da39f2a00bcdc0463c0ac5bd550300
Harel Mazor <harel.mazor@gmail.com>
2018-09-10 22:51:09 +0300

    Build android version on every commit #788 - fix hang according to appveyor issue. added IHM android key.

--- Scripts/PostBuildPackArtifacts.ps1
+++ Scripts/PostBuildPackArtifacts.ps1
@@ -25,20 +25,20 @@ Push-AppveyorArtifact $artifactsFileName
 
 # Building android:
 
-$buildAndroidClient = "npm run build -- -c android --no-progress"
-Write-Host $buildAndroidClient
-Invoke-Expression $buildAndroidClient
+#$buildAndroidClient = "npm run build -- -c android --no-progress"
+#Write-Host $buildAndroidClient
+#Invoke-Expression $buildAndroidClient
 
-$AddAndroid = "npm run add-android"
-Write-Host $AddAndroid
-Invoke-Expression $AddAndroid
+#$AddAndroid = "npm run add-android"
+#Write-Host $AddAndroid
+#Invoke-Expression $AddAndroid
 
-$buildApk = "npm run build-apk"
-Write-Host $buildApk
-Invoke-Expression $buildApk
+#$buildApk = "npm run build-apk"
+#Write-Host $buildApk
+#Invoke-Expression $buildApk
 
 Write-Host "Finished building apk"
 
-Push-AppveyorArtifact .\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk
+Push-AppveyorArtifact .\platforms\android\app\build\outputs\apk\release\app-release.apk
 
 Set-Location -Path $env:APPVEYOR_BUILD_FOLDER
