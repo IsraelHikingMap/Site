a8c3691811fbb1f999aa8a58ae550cb1221945d1
Harel Mazor <harel.mazor@gmail.com>
2018-11-07 00:22:46 +0200

    Support map rotation #669 - add zoom animation for zoom-in/out

--- IsraelHiking.Web/sources/application/components/zoom.component.ts
+++ IsraelHiking.Web/sources/application/components/zoom.component.ts
@@ -1,11 +1,8 @@
 import { Component } from "@angular/core";
-import { MapComponent } from "ngx-openlayers";
-import { NgRedux } from "@angular-redux/store";
 
 import { BaseMapComponent } from "./base-map.component";
 import { ResourcesService } from "../services/resources.service";
-import { LocationActions } from "../reducres/location.reducer";
-import { ApplicationState } from "../models/models";
+import { MapService } from "../services/map.service";
 
 @Component({
     selector: "zoom",
@@ -13,14 +10,20 @@ import { ApplicationState } from "../models/models";
 })
 export class ZoomComponent extends BaseMapComponent {
     constructor(resources: ResourcesService,
-        private readonly ngRedux: NgRedux<ApplicationState>) {
+        private readonly mapService: MapService) {
         super(resources);
     }
     public zoomIn() {
-        this.ngRedux.dispatch(LocationActions.zoomInAction);
+        this.mapService.map.getView().animate({
+            zoom: Math.round(this.mapService.map.getView().getZoom()) + 1,
+            duration: 250
+        });
     }
 
     public zoomOut() {
-        this.ngRedux.dispatch(LocationActions.zoomOutAction);
+        this.mapService.map.getView().animate({
+            zoom: Math.round(this.mapService.map.getView().getZoom()) - 1,
+            duration: 250
+        });
     }
 }
--- IsraelHiking.Web/sources/application/reducres/location.reducer.ts
+++ IsraelHiking.Web/sources/application/reducres/location.reducer.ts
@@ -1,26 +1,13 @@
-import { Action, AnyAction, Reducer } from "redux";
 import { Location } from "../models/models";
 import { initialState } from "./initial-state";
 import { ReduxAction, BaseAction, createReducerFromClass } from "./reducer-action-decorator";
 
-const ZOOM_IN = "ZOOM_IN";
-const ZOOM_OUT = "ZOOM_OUT";
 const SET_LOCATION = "SET_LOCATION";
 
 export interface SetLocationPayload {
     longitude: number;
     latitude: number;
     zoom?: number;
-}
-
-export class LocationActions {
-    public static readonly zoomInAction: Action = {
-        type: ZOOM_IN
-    };
-
-    public static readonly zoomOutAction: Action = {
-        type: ZOOM_OUT
-    };
 }
 
 export class SetLocationAction extends BaseAction<SetLocationPayload> {
@@ -30,22 +17,6 @@ export class SetLocationAction extends BaseAction<SetLocationPayload> {
 }
 
 export class LocationReducer {
-    @ReduxAction(ZOOM_IN)
-    public zoomIn(lastState: Location, action: Action) {
-        return {
-            ...lastState,
-            zoom: lastState.zoom + 1,
-        };
-    }
-
-    @ReduxAction(ZOOM_OUT)
-    public zoomOut(lastState: Location, action: Action) {
-        return {
-            ...lastState,
-            zoom: lastState.zoom - 1,
-        };
-    }
-
     @ReduxAction(SET_LOCATION)
     public setLocation(lastState: Location, action: SetLocationAction) {
         return {
