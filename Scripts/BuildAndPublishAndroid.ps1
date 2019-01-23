086d146b2f2b9fce5f0d1ce4cc858a877d2eab86
Harel Mazor <harel.mazor@gmail.com>
2019-01-23 10:22:30 +0200

    #820 - Fix build script

--- Scripts/BuildAndPublishAndroid.ps1
+++ Scripts/BuildAndPublishAndroid.ps1
@@ -38,13 +38,14 @@ Write-Host "npm run build-apk"
 npm run build-apk
 
 $apkVersioned = ".\IHM_signed_$env:APPVEYOR_BUILD_VERSION.apk"
+$preSignApkLocation = ".\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk";
 
-if (-not (Test-Path -Path $apkVersioned)) {
+if (-not (Test-Path -Path $preSignApkLocation)) {
 	throw "Failed to create android apk file"
 }
 
 Write-Host "Signing apk"
-Invoke-Expression "& ""$env:ANDROID_HOME\build-tools\28.0.2\apksigner.bat"" sign --ks .\IHM.jks --ks-pass pass:$env:STORE_PASSWORD --key-pass pass:$env:PASSWORD --out $apkVersioned .\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk"
+Invoke-Expression "& ""$env:ANDROID_HOME\build-tools\28.0.2\apksigner.bat"" sign --ks .\IHM.jks --ks-pass pass:$env:STORE_PASSWORD --key-pass pass:$env:PASSWORD --out $apkVersioned $preSignApkLocation"
 
 Push-AppveyorArtifact $apkVersioned
 
