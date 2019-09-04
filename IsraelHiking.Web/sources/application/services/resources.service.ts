edea7845b76146eb0a18f89cfe9a719d285f849e
Harel Mazor <harel.mazor@gmail.com>
2019-09-04 20:50:20 +0300

    Crowd funding banner #1042 - undo stuff that were missed.

--- IsraelHiking.Web/sources/application/services/resources.service.ts
+++ IsraelHiking.Web/sources/application/services/resources.service.ts
@@ -498,7 +498,7 @@ export class ResourcesService {
     public setLanguage = async (language: ILanguage): Promise<any> => {
         this.setRtl(language.rtl);
         this.gettextCatalog.setCurrentLanguage(language.code);
-        await this.gettextCatalog.loadRemote(Urls.translations + language.code + ".json?sign=1567456153422");
+        await this.gettextCatalog.loadRemote(Urls.translations + language.code + ".json?sign=1567619189564");
 
         this.about = this.gettextCatalog.getString("About");
         this.help = this.gettextCatalog.getString("Help");
