f020e7ff7d6b2e30b37aed1e55347e083abff340
HarelM <harel.mazor@gmail.com>
2019-12-18 22:18:47 +0200

    Fixes #947 Optimize energy management - Added original brightness variable.

--- IsraelHiking.Web/sources/application/services/screen.service.ts
+++ IsraelHiking.Web/sources/application/services/screen.service.ts
@@ -4,15 +4,19 @@ import { Idle, DEFAULT_INTERRUPTSOURCES } from "@ng-idle/core";
 
 import { RunningContextService } from "./running-context.service";
 import { ApplicationState } from "../models/models";
+import { LoggingService } from './logging.service';
 
 declare var cordova: any;
 
 @Injectable()
 export class ScreenService {
 
+    private originalBrightness: number;
+
     constructor(private readonly runningContextService: RunningContextService,
                 private readonly ngRedux: NgRedux<ApplicationState>,
-                private readonly userIdleService: Idle) { }
+                private readonly userIdleService: Idle,
+                private readonly logger: LoggingService) { }
 
     public initialize() {
         if (!this.runningContextService.isCordova) {
@@ -20,17 +24,17 @@ export class ScreenService {
         }
         let brightness = cordova.plugins.brightness;
         brightness.setKeepScreenOn(true);
+        brightness.getBrightness((currentBrightness) => this.originalBrightness = currentBrightness);
         document.addEventListener("resume", () => {
             brightness.setKeepScreenOn(true);
         }, false);
-        if (this.runningContextService.isIos) {
-            return;
-        }
         document.addEventListener("resume", () => {
-            brightness.setBrightness(-1);
+            this.logger.debug(`Resume app, setting brightness to original: ${this.originalBrightness}`);
+            brightness.setBrightness(this.originalBrightness);
             this.userIdleService.watch();
         }, false);
         document.addEventListener("pause", () => {
+            this.logger.debug("Pausing app, stopping user idle service.");
             this.userIdleService.stop();
         }, false);
         this.userIdleService.setInterrupts(DEFAULT_INTERRUPTSOURCES);
@@ -38,12 +42,14 @@ export class ScreenService {
         this.userIdleService.setTimeout(false);
         this.userIdleService.onIdleStart.subscribe(() => {
             if (this.ngRedux.getState().configuration.isBatteryOptimization) {
+                this.logger.debug("User is idle, setting brightness to 0.01");
                 brightness.setBrightness(0.01);
             }
         });
         this.userIdleService.onIdleEnd.subscribe(() => {
             if (this.ngRedux.getState().configuration.isBatteryOptimization) {
-                brightness.setBrightness(-1);
+                this.logger.debug(`User is active, setting brightness to original: ${this.originalBrightness}`);
+                brightness.setBrightness(this.originalBrightness);
             }
         });
         this.userIdleService.watch();
