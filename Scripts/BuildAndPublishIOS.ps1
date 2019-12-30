30e53bd74741753f98a14ef61e49c193cce8906a
HarelM <harel.mazor@gmail.com>
2019-12-30 23:29:28 +0200

    Make iOS builds automatic #1112 - Added provision and certificates

--- Scripts/BuildAndPublishIOS.ps1
+++ Scripts/BuildAndPublishIOS.ps1
@@ -25,6 +25,34 @@ $xml.Save($filePath)
 Write-Host "npm run add-ios"
 npm run add-ios
 
+Write-Host "Decripting files"
+openssl aes-256-cbc -k pass:$env:PASSWORD -in ./certificates/appveyor.mobileprovision.enc -d -a -out ./certificates/appveyor.mobileprovision.enc
+openssl aes-256-cbc -k pass:$env:PASSWORD -in ./certificates/ihm-dist.cer.enc -d -a -out ./certificates/dist.cer
+openssl aes-256-cbc -k pass:$env:PASSWORD -in ./certificates/ihm-dist.p12.enc -d -a -out ./certificates/dist.p12
+
+
+Write-Host "Create a custom keychain"
+security create-keychain -p appveyor ios-build.keychain
+
+Write-Host "Make the custom keychain default, so xcodebuild will use it for signing"
+security default-keychain -s ios-build.keychain
+
+Write-Host "Unlock the keychain"
+security unlock-keychain -p appveyor ios-build.keychain
+
+# Set keychain timeout to 1 hour for long builds
+# see http://www.egeek.me/2013/02/23/jenkins-and-xcode-user-interaction-is-not-allowed/
+security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
+
+Write-Host "Add certificates to keychain and allow codesign to access them"
+security import ./c/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./certificates/ihm-dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./certificates/ihm-dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+
+
+New-Item -ItemType "directory" -Path "~/Library/MobileDevice/Provisioning Profiles" -Verbose
+Copy-Item "./certificates/appveyor.mobileprovision" -Destination "~/Library/MobileDevice/Provisioning Profiles/" -Verbose
+
 Write-Host "npm run build-ipa"
 npm run build-ipa
 
