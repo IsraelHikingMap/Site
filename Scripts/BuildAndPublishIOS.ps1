66059299856d2d4e569a1098ffffeb95bb866cd3
Harel Mazor <harel.mazor@gmail.com>
2019-12-31 00:09:53 +0200

    Make iOS builds automatic #1112 - another attempt to fix openssl command

--- Scripts/BuildAndPublishIOS.ps1
+++ Scripts/BuildAndPublishIOS.ps1
@@ -1,12 +1,5 @@
 Set-Location -Path "$($env:APPVEYOR_BUILD_FOLDER)\IsraelHiking.Web"
 
-# Building android:
-Write-Host "npm install --loglevel=error"
-npm install --loglevel=error
-
-Write-Host "increase-memory-limit"
-increase-memory-limit
-
 
 #Replace version in config.xml file
 $filePath = get-ChildItem config.xml | Select-Object -first 1 | select -expand FullName
@@ -15,14 +8,10 @@ $xml.Load($filePath)
 $xml.widget.version = $env:APPVEYOR_BUILD_VERSION
 $xml.Save($filePath)
 
-Write-Host "npm run add-ios"
-npm run add-ios
-
 Write-Host "Decripting files"
-Invoke-Expression "& openssl aes-256-cbc -k pass:$env:PASSWORD -in ./certificates/appveyor.mobileprovision.enc -d -a -out ./certificates/appveyor.mobileprovision.enc"
-Invoke-Expression "& openssl aes-256-cbc -k pass:$env:PASSWORD -in ./certificates/ihm-dist.cer.enc -d -a -out ./certificates/dist.cer"
-Invoke-Expression "& openssl aes-256-cbc -k pass:$env:PASSWORD -in ./certificates/ihm-dist.p12.enc -d -a -out ./certificates/dist.p12"
-
+Invoke-Expression "& openssl aes-256-cbc -k ""pass:$env:PASSWORD"" -in ./certificates/appveyor.mobileprovision.enc -d -a -out ./certificates/appveyor.mobileprovision.enc"
+Invoke-Expression "& openssl aes-256-cbc -k ""pass:$env:PASSWORD"" -in ./certificates/ihm-dist.cer.enc -d -a -out ./certificates/dist.cer"
+Invoke-Expression "& openssl aes-256-cbc -k ""pass:$env:PASSWORD"" -in ./certificates/ihm-dist.p12.enc -d -a -out ./certificates/dist.p12"
 
 Write-Host "Create a custom keychain"
 security create-keychain -p appveyor ios-build.keychain
@@ -42,9 +31,18 @@ security import ./certificates/apple.cer -k ~/Library/Keychains/ios-build.keycha
 security import ./certificates/ihm-dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./certificates/ihm-dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 
-
 New-Item -ItemType "directory" -Path "~/Library/MobileDevice/Provisioning Profiles" -Verbose
 Copy-Item "./certificates/appveyor.mobileprovision" -Destination "~/Library/MobileDevice/Provisioning Profiles/" -Verbose
+
+# Building ios:
+Write-Host "npm install --loglevel=error"
+npm install --loglevel=error
+
+Write-Host "increase-memory-limit"
+increase-memory-limit
+
+Write-Host "npm run add-ios"
+npm run add-ios
 
 Write-Host "npm run build:cordova -- --no-progress"
 npm run build:cordova -- --no-progress
@@ -61,7 +59,7 @@ $ipaVersioned = ".\IHM_signed_$env:APPVEYOR_BUILD_VERSION.ipa"
 #$preSignIpaLocation = ".\platforms\ios\app\build\outputs\ipa\release\app-release-unsigned.apk";
 
 #if (-not (Test-Path -Path $preSignIpaLocation)) {
-#	throw "Failed to create android apk file"
+#	throw "Failed to create ios apk file"
 #}
 
 #Push-AppveyorArtifact $apkVersioned
