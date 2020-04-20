520881fcd027f8b1a901ede6e8926b8d11ccedc1
Harel Mazor <harel.mazor@gmail.com>
2020-04-20 11:34:15 +0300

    Migrate to docker #641 - Small fixes to config and rebuild script.

--- gh-update.ps1
+++ gh-update.ps1
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# The following line basically does all the magic using bash -c: 
 # 1. It run the container, imports graphhopper data to a temporay directory (new-gh)
-# 2. Deleted the default cache (default-gh) 
+docker-compose run --rm --entrypoint /bin/bash graphhopper -c "./graphhopper.sh import asia_israel-and-palestine.pbf -c /data/gh-config.yml -fd -o /data/new-gh/"
+# 2. Deleted the default cache (default-gh)
+docker-compose run --rm --entrypoint /bin/bash graphhopper -c "rm -rf /data/default-gh/"
 # 3. Moves the temporary cache to the default location.
-docker-compose run --rm --entrypoint /bin/bash graphhopper -c "./graphhopper.sh import asia_israel-and-palestine.pbf -c /data/gh-config.yml -fd -o /data/new-gh/ && rm -rf /data/default-gh/ && mv /data/new-gh /data/default-gh"
-# When everything is done, restarting the container will make it use the updated default cache.
+docker-compose run --rm --entrypoint /bin/bash graphhopper -c "mv /data/new-gh /data/default-gh"
+# 4. Restart the container will make it use the updated default cache.
 docker-compose restart graphhopper
