3ae8d46829d31a849180c3d2d55f127ef62777e0
Harel Mazor <harel.mazor@gmail.com>
2020-06-27 21:55:12 +0300

    Update report a problem mail text.

--- IsraelHiking.Web/sources/application/services/resources.service.ts
+++ IsraelHiking.Web/sources/application/services/resources.service.ts
@@ -531,7 +531,7 @@ export class ResourcesService {
     public setLanguage = async (language: ILanguage): Promise<void> => {
         this.setRtl(language.rtl);
         this.gettextCatalog.setCurrentLanguage(language.code);
-        await this.gettextCatalog.loadRemote(Urls.translations + language.code + ".json?sign=1593235204483");
+        await this.gettextCatalog.loadRemote(Urls.translations + language.code + ".json?sign=1593283240167");
 
         this.about = this.gettextCatalog.getString("About");
         this.help = this.gettextCatalog.getString("Help");
