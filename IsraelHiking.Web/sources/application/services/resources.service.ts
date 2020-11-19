833952730445244dbad1fc7b3c2dd4ede2397d2a
HarelM <harel.mazor@gmail.com>
2020-11-19 20:23:29 +0200

    Terminology change: share/save -> save/export #1352 - Change export translation

--- IsraelHiking.Web/sources/application/services/resources.service.ts
+++ IsraelHiking.Web/sources/application/services/resources.service.ts
@@ -453,7 +453,7 @@ export class ResourcesService {
     public setLanguage = async (language: ILanguage): Promise<void> => {
         this.setRtl(language.rtl);
         this.gettextCatalog.setCurrentLanguage(language.code);
-        await this.gettextCatalog.loadRemote(Urls.translations + language.code + ".json?sign=1605781587939");
+        await this.gettextCatalog.loadRemote(Urls.translations + language.code + ".json?sign=1605810093272");
 
         this.about = this.gettextCatalog.getString("About");
         this.legend = this.gettextCatalog.getString("Legend");
