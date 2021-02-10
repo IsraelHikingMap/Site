5114fce4c2ec7b1d1c4f435bd2ea483cda32f1f0
HarelM <harel.mazor@gmail.com>
2021-02-10 21:55:30 +0200

    Upgrade angular #1412 - move file-saver to be use es compilation.

--- IsraelHiking.Web/src/application/services/non-angular-objects.factory.ts
+++ IsraelHiking.Web/src/application/services/non-angular-objects.factory.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { saveAs, FileSaverOptions } from "file-saver";
+import { saveAs, FileSaverOptions } from "file-saver-es";
 import * as ohauth from "ohauth";
 
 export interface IOAuthResponse {
