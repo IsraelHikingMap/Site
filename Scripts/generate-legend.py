abc82b006b192bba79552555467739d8da51a0b6
zstadler <zeev.stadler@gmail.com>
2021-04-06 11:29:44 +0300

    Update legend (#1470)
    Resolve #1468
    
    Spelling correction in Scripts/BuildAndPublishIOS.ps1

--- Scripts/generate-legend.py
+++ Scripts/generate-legend.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
-#######################################################
-# Make sure to run this script on the server and make #
-# sure legend.json file is right next to this script. #
-#######################################################
+'''
+This script generates the legend thumbnails for IHM and ilMTB styles
+
+Usage:
+    generate-legend.py [substring]
+
+    If substring is given, only files with names containing the substring will be generated
+
+Workflow:
+1. `make refresh-styles start-tileserver-legend` in the vector tile server directory
+2. `cd IsraelHiking.Web/src/content/legend` in this repository
+3. Optional: `rm *.png` to clean unused legend entries
+4. `../../../../Scripts/generate-legend.py`
+'''
 
 import json
 import requests
@@ -21,17 +31,21 @@ with open("legend.json", "r", encoding="utf8") as read_file:
 
 styles = ["IHM", "ilMTB"]
 
+sys.argv.append("")
+# sys.stderr.write("sys.argv[1]: {}\n".format(sys.argv[1]))
+
 for style in styles:
     for section in jsonData:
         for item in section["items"]:
             width = 50 if item["type"] == "POI" else 200
-            uri = "http://10.10.10.11:8080/styles/{}/static/{},{},{}/{}x50@2x.png".format(
+            uri = "http://0.0.0.0:8082/styles/{}/static/{:.7f},{:.7f},{:02}/{:03}x50@2x.png".format(
                     style, item["latlng"]["lng"], item["latlng"]["lat"], item["zoom"], width)
             filename = "{0}_{1}.png".format(style, item["key"])
-            sys.stderr.write("{}\t-> {} ".format(uri, filename))
-            sys.stderr.flush()
-            with open(filename, "wb") as f:
-                response = session.get(uri)
-                sys.stderr.write("{}\n".format(response.status_code))
-                f.write(response.content)
+            if sys.argv[1] in filename:
+                sys.stderr.write("{}\t-> {} ".format(uri, filename))
+                sys.stderr.flush()
+                with open(filename, "wb") as f:
+                    response = session.get(uri)
+                    sys.stderr.write("{}\n".format(response.status_code))
+                    f.write(response.content)
 
