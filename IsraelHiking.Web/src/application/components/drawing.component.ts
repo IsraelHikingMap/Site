04864081355d1fdb81bc377864c3d69077416dd2
harel <harel.mazor@gmail.com>
2021-04-10 14:34:17 +0300

    Add Ctrl+Y for redo #1476 - better support for mac users

--- IsraelHiking.Web/src/application/components/drawing.component.ts
+++ IsraelHiking.Web/src/application/components/drawing.component.ts
@@ -36,12 +36,13 @@ export class DrawingComponent extends BaseMapComponent {
 
     @HostListener("window:keydown", ["$event"])
     public onDrawingShortcutKeys($event: KeyboardEvent) {
-        if ($event.ctrlKey && $event.key.toLowerCase() === "z") {
-            this.undo();
+        if (($event.ctrlKey && $event.key.toLowerCase() === "y") ||
+            ($event.metaKey && $event.shiftKey && $event.key.toLowerCase() === "z")) {
+            this.redo();
             return;
         }
-        if ($event.ctrlKey && $event.key.toLowerCase() === "y") {
-            this.redo();
+        if (($event.ctrlKey || $event.metaKey) && $event.key.toLowerCase() === "z") {
+            this.undo();
             return;
         }
         if (this.selectedRouteService.getSelectedRoute() == null) {
