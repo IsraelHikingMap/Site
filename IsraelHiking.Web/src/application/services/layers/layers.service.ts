779c07ddb7c449797eb888c96aa3acf3cb924399
zstadler <zeev.stadler@gmail.com>
2021-05-28 16:35:35 +0300

    Fix case sensitivity in OSM editing address

--- IsraelHiking.Web/src/application/services/layers/layers.service.ts
+++ IsraelHiking.Web/src/application/services/layers/layers.service.ts
@@ -96,7 +96,7 @@ export class LayersService {
         if (baseLayerAddress.indexOf("{x}") !== -1) {
             return baseLayerAddress;
         }
-        let defaultAddress = Urls.baseTilesAddress + "/Hebrew/tiles/{z}/{x}/{y}.png";
+        let defaultAddress = Urls.baseTilesAddress + "/Hebrew/Tiles/{z}/{x}/{y}.png";
         // using the same logic that the server is using in ImageCreationService + language
         if (!baseLayerAddress) {
             return defaultAddress;
@@ -106,7 +106,7 @@ export class LayersService {
         if (baseLayerAddress.endsWith(".json")) {
             let styleKey = baseLayerAddress.replace(".json", "").split("/").splice(-1)[0];
             if (styleKey === "ilMTB") {
-                tiles = "mtbtiles";
+                tiles = "mtbTiles";
             }
         }
         return `${Urls.baseTilesAddress}/${language}/${tiles}/{z}/{x}/{y}.png`;
