eec1e9adfa60042bc38d180ea61e0dd7a41f622e
harel <harel.mazor@gmail.com>
2021-07-07 00:07:47 +0300

    Resolves #1545 - Upgrade to karma-coverage as istanbuld is deprected

--- IsraelHiking.Web/karma.conf.js
+++ IsraelHiking.Web/karma.conf.js
@@ -9,19 +9,23 @@ module.exports = function (config) {
             require("karma-jasmine"),
             require("karma-chrome-launcher"),
             require("karma-jasmine-html-reporter"),
-            require("karma-coverage-istanbul-reporter"),
+            require("karma-coverage"),
             require("karma-junit-reporter"),
             require("@angular-devkit/build-angular/plugins/karma")
         ],
         client: {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
-        coverageIstanbulReporter: {
-            dir: require("path").join(__dirname, "coverage"), reports: ["html", "lcovonly"],
-            fixWebpackSourcePaths: true
-        },
+        coverageReporter: {
+            dir: require("path").join(__dirname, "coverage"),
+            subdir: ".",
+            reporters: [
+              { type: "html" },
+              { type: "lcovonly" }
+            ]
+          },
         
-        reporters: ["progress", "kjhtml", "junit", "coverage-istanbul"],
+        reporters: ["progress", "kjhtml", "junit"],
         port: 9876,
         colors: true,
         captureTimeout: 300000,
