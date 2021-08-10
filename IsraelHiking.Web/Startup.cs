ebadf9301da309573ae21321ff73eced78f95db8
harel <harel.mazor@gmail.com>
2021-08-10 22:48:56 +0300

    Allow Let's encrypt access to acme-challenge #1563 - Attempt to fix.

--- IsraelHiking.Web/Startup.cs
+++ IsraelHiking.Web/Startup.cs
@@ -137,6 +137,18 @@ namespace IsraelHiking.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // serve https certificate folder
+            var wellKnownFolder = Path.Combine(Directory.GetCurrentDirectory(), ".well-known");
+            if (Directory.Exists(wellKnownFolder))
+            {
+                app.UseStaticFiles(new StaticFileOptions
+                {
+                    FileProvider = new PhysicalFileProvider(wellKnownFolder),
+                    RequestPath = new PathString("/.well-known"),
+                    ServeUnknownFileTypes = true, // serve extensionless file
+                    DefaultContentType = "application/json"
+                });
+            }
             if (_isDevelopment)
             {
                 app.UseDeveloperExceptionPage();
@@ -238,18 +250,7 @@ namespace IsraelHiking.Web
                 };
                 app.UseFileServer(fileServerOptions);
             }
-            // serve https certificate folder
-            var wellKnownFolder = Path.Combine(Directory.GetCurrentDirectory(), ".well-known");
-            if (Directory.Exists(wellKnownFolder))
-            {
-                app.UseStaticFiles(new StaticFileOptions
-                {
-                    FileProvider = new PhysicalFileProvider(wellKnownFolder),
-                    RequestPath = new PathString("/.well-known"),
-                    ServeUnknownFileTypes = true, // serve extensionless file
-                    DefaultContentType = "application/json"
-                });
-            }
+
             // wwwroot
             app.UseStaticFiles(new StaticFileOptions
             {
