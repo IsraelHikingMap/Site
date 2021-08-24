c0f21aa543841b279cafed4b1b302ce05d1601d9
arielhasidim <23483971+arielhasidim@users.noreply.github.com>
2021-08-24 16:56:05 +0300

    Added lottie support + embeded 4 animation for intro-dialog

--- IsraelHiking.Web/src/application/components/dialogs/intro-dialog.component.ts
+++ IsraelHiking.Web/src/application/components/dialogs/intro-dialog.component.ts
@@ -6,11 +6,28 @@ import { NgRedux } from "../../reducers/infra/ng-redux.module";
 import { ResourcesService } from "../../services/resources.service";
 import { ApplicationState } from "../../models/models";
 import { ConfigurationActions } from "application/reducers/configuration.reducer";
+
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
 @Component({
     selector: "intro-dialog",
-    templateUrl: "./intro-dialog.component.html"
+    templateUrl: "./intro-dialog.component.html",
+    styleUrls: ["./intro-dialog.component.scss"]
 })
 export class IntroDialogComponent extends BaseMapComponent {
+
+    lottieLanguage: AnimationOptions = {
+        path: '../../../content/lottie/dialog-language.json',
+    };
+    lottieMaps: AnimationOptions = {
+        path: '../../../content/lottie/dialog-maps.json',
+    };
+    lottiePlan: AnimationOptions = {
+        path: '../../../content/lottie/dialog-plan.json',
+    };
+    lottieMore: AnimationOptions = {
+        path: '../../../content/lottie/dialog-more.json',
+    };
 
     public step: number;
 
