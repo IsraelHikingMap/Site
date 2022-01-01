f08507f266c707af7c513fc54d35c9e9466905d7
harel <harel.mazor@gmail.com>
2022-01-01 14:37:28 +0200

    Improve code coverage #1548 - Add coverage for external source updater

---
+++ Tests/IsraelHiking.API.Tests/Executors/ExternalSourceUpdaterExecutorTests.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using IsraelHiking.API.Executors;
+using IsraelHiking.API.Services.Poi;
+using IsraelHiking.Common.Extensions;
+using IsraelHiking.DataAccessInterfaces;
+using IsraelHiking.DataAccessInterfaces.Repositories;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetTopologySuite.Features;
+using NetTopologySuite.Geometries;
+using NSubstitute;
+
+namespace IsraelHiking.API.Tests.Executors
+{
+    [TestClass]
+    public class ExternalSourceUpdaterExecutorTests
+    {
+        private ExternalSourceUpdaterExecutor _executor;
+        private IPointsOfInterestAdapterFactory _pointsOfInterestAdapterFactory;
+        private IElevationGateway _elevationGateway;
+        private IExternalSourcesRepository _externalSourcesRepository;
+        
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _pointsOfInterestAdapterFactory = Substitute.For<IPointsOfInterestAdapterFactory>();
+            _elevationGateway = Substitute.For<IElevationGateway>();
+            _externalSourcesRepository = Substitute.For<IExternalSourcesRepository>();
+            _executor = new ExternalSourceUpdaterExecutor(_pointsOfInterestAdapterFactory, 
+                _elevationGateway,
+                _externalSourcesRepository, 
+                new ItmWgs84MathTransfromFactory(), 
+                Substitute.For<ILogger>());
+        }
+
+        [TestMethod]
+        public void UpdateSource_NoPreviousPoints_ShouldUpdate()
+        {
+            const string sourceName = "sourceName";
+            var adapter = Substitute.For<IPointsOfInterestAdapter>();
+            adapter.GetUpdates(Arg.Any<DateTime>()).Returns(new List<Feature>());
+            _pointsOfInterestAdapterFactory.GetBySource(sourceName).Returns(adapter);
+            _externalSourcesRepository.GetExternalPoisBySource(sourceName).Returns(new List<Feature>());
+            
+            _executor.UpdateSource(sourceName).Wait();
+
+            _externalSourcesRepository.Received(1).AddExternalPois(Arg.Any<List<Feature>>());
+        }
+        
+        [TestMethod]
+        public void UpdateSource_UseLastModified_ShouldUpdate()
+        {
+            const string sourceName = "sourceName";
+            var feature = new Feature(new Point(0, 0), new AttributesTable());
+            feature.SetLastModified(new DateTime(0));
+            var adapter = Substitute.For<IPointsOfInterestAdapter>();
+            adapter.GetUpdates(Arg.Any<DateTime>()).Returns(new List<Feature>());
+            _pointsOfInterestAdapterFactory.GetBySource(sourceName).Returns(adapter);
+            _externalSourcesRepository.GetExternalPoisBySource(sourceName).Returns(new List<Feature> {feature});
+            
+            _executor.UpdateSource(sourceName).Wait();
+
+            _externalSourcesRepository.Received(1).AddExternalPois(Arg.Any<List<Feature>>());
+        }
+
+        [TestMethod]
+        public void RebuildSource_ShouldRebuild()
+        {
+            const string sourceName = "sourceName";
+            var adapter = Substitute.For<IPointsOfInterestAdapter>();
+            var feature = new Feature(new Point(0, 0), new AttributesTable());
+            feature.SetLocation(new Coordinate(0,0));
+            adapter.GetAll().Returns(new List<Feature> { feature});
+            _elevationGateway.GetElevation(Arg.Any<Coordinate[]>()).Returns(new [] {0.0});
+            _pointsOfInterestAdapterFactory.GetBySource(sourceName).Returns(adapter);
+
+            _executor.RebuildSource(sourceName).Wait();
+            
+            _externalSourcesRepository.Received(1).DeleteExternalPoisBySource(sourceName);
+            _externalSourcesRepository.Received(1).AddExternalPois(Arg.Any<List<Feature>>());
+        }
+    }
+}
