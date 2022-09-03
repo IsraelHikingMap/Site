a704d3b29329ac7122831182e9b1558483341e19
harel <harel.mazor@gmail.com>
2022-09-03 22:26:41 +0300

    Fixes #1753 - File uh-oh.mp3 not found

--- IsraelHiking.Web/src/application/services/audio-player.factory.ts
+++ IsraelHiking.Web/src/application/services/audio-player.factory.ts
@@ -1,15 +1,21 @@
 import { Injectable } from "@angular/core";
 import { NativeAudio } from "@capacitor-community/native-audio";
 
+import { RunningContextService } from "./running-context.service";
+
 export interface IAudioPlayer {
     play(): void;
 }
 
 @Injectable()
 export class AudioPlayerFactory {
-    constructor() { }
+    constructor(private readonly runningContext: RunningContextService) { }
 
     public async create(): Promise<IAudioPlayer> {
+        if (!this.runningContext.isCapacitor) {
+            return new Audio("content/uh-oh.mp3");
+        }
+
         await NativeAudio.preload({
             assetId: "uh-oh",
             assetPath: "public/content/uh-oh.mp3",
