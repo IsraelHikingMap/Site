93a6e61b7efdedfb9916f98e477990c718f7c639
harel <harel.mazor@gmail.com>
2023-03-02 10:44:32 +0200

    #1840 - Missing images in the offline POIs file - remove image in case of empty image in csv

--- Tests/IsraelHiking.API.Tests/Services/Poi/CsvPointsOfInterestAdapterTests.cs
+++ Tests/IsraelHiking.API.Tests/Services/Poi/CsvPointsOfInterestAdapterTests.cs
@@ -7,7 +7,9 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NetTopologySuite.Features;
 using NSubstitute;
 using System;
+using System.Linq;
 using System.Text;
+using IsraelHiking.Common;
 
 namespace IsraelHiking.API.Tests.Services.Poi
 {
@@ -17,9 +19,9 @@ namespace IsraelHiking.API.Tests.Services.Poi
         private CsvPointsOfInterestAdapter _adapter;
         private IRemoteFileFetcherGateway _remoteFileFetcherGateway;
 
-        private void SetupFileStream(string address)
+        private void SetupFileStream(string address, string content = null)
         {
-            var bytes = Encoding.UTF8.GetBytes(
+            var bytes = Encoding.UTF8.GetBytes(content ??
                 "Id,Title,Description,Website,ImageUrl,SourceImageUrl,Category,FileUrl,Icon,IconColor,Latitude,Longitude,LastModified\r\n1,2,3,4,5,6,7,8,9,0,1,2,2013-3-31");
             _remoteFileFetcherGateway.GetFileContent(address).Returns(new RemoteFileFetcherGatewayResponse
             {
@@ -53,9 +55,28 @@ namespace IsraelHiking.API.Tests.Services.Poi
 
             Assert.AreEqual(1, features.Count);
         }
+        
+        public void GetAll_MissingImage_ShouldReturnOnePointWithNoImageField()
+        {
+            var address = "http://csv.csv";
+            _adapter.SetFileNameAndAddress("csv.csv", address);
+            SetupFileStream(address,
+                "Id,Title,Description,Website,ImageUrl,SourceImageUrl,Category,FileUrl,Icon,IconColor,Latitude,Longitude,LastModified\r\n1,2,3,4,,6,7,8,9,0,1,2,2013-3-31");
+            _remoteFileFetcherGateway.GetFileContent("8").Returns(new RemoteFileFetcherGatewayResponse
+            {
+                Content = new FeatureCollection().ToBytes(),
+                FileName = "csv.geojson"
+            });
+            _dataContainerConverterService.Convert(Arg.Any<byte[]>(), Arg.Any<string>(), Arg.Any<string>()).Returns(new FeatureCollection().ToBytes());
+
+            var features = _adapter.GetAll().Result;
+
+            Assert.AreEqual(1, features.Count);
+            Assert.IsFalse(features.First().Attributes.Exists(FeatureAttributes.IMAGE_URL));
+        }
 
         [TestMethod]
-        public void GetLastModifed_ShouldReturnNone()
+        public void GetLastModified_ShouldReturnNone()
         {
             var address = "http://csv.csv";
             _adapter.SetFileNameAndAddress("csv.csv", address);
