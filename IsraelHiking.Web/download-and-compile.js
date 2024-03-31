3501e89c52fa0d1021cfea98839cfeb269a171c7
HarelM <harel.mazor@gmail.com>
2024-03-31 16:59:34 +0300

    Improve emojy support for translated text

--- IsraelHiking.Web/download-and-compile.js
+++ IsraelHiking.Web/download-and-compile.js
@@ -8,42 +8,39 @@ var englishFilePath = "./src/translations/en-Us.po";
 var hebrewFilePath = "./src/translations/he.po";
 var resourcesFilePath = "./src/application/services/resources.service.ts";
 
-console.log("Downloading English translation");
-https.get("https://translate.zanata.org/rest/file/translation/IsraelHiking/Main/en-US/po?docId=IsraelHiking",
-    (response) => {
-        var englishStream = response.pipe(fs.createWriteStream(englishFilePath));
-        englishStream.on("finish",
-            () => {
-                po2json.parseFile(englishFilePath,
-                    { pretty: true, format: "mf" },
-                    (_, json) => {
-                        fs.writeFileSync(englishFilePath.replace(".po", ".json"), JSON.stringify(json, null, 4));
-                        console.log("Finished English translation compilation to JSON");
-                    });
+function downloadAndProcess(languageCode, filePath) {
+    console.log(`Downloading ${languageCode} translation`);
+    https.get(`https://translate.zanata.org/rest/file/translation/IsraelHiking/Main/${languageCode}/po?docId=IsraelHiking`,
+        (response) => {
+        const englishStream = response.pipe(fs.createWriteStream(filePath));
+            englishStream.on("finish",
+                () => {
+                    po2json.parseFile(filePath,
+                        { pretty: true, format: "mf" },
+                        (_, json) => {
+                            for (let key in json) {
+                                json[key] = json[key].replace(":-)", "😊");
+                                json[key] = json[key].replace(":-(", "😞");
+                            }
+                            const jsonFilePath = filePath.replace(".po", ".json");
+                            fs.writeFileSync(jsonFilePath, JSON.stringify(json, null, 4));
+                            console.log("Finished translation compilation to JSON " + jsonFilePath);
+                        });
+                });
             });
-        console.log("Downloading Hebrew translation");
-        https.get("https://translate.zanata.org/rest/file/translation/IsraelHiking/Main/he/po?docId=IsraelHiking",
-            (response) => {
-                var hebrewStream = response.pipe(fs.createWriteStream(hebrewFilePath));
-                hebrewStream.on("finish",
-                    () => {
-                        po2json.parseFile(hebrewFilePath,
-                            { pretty: true, format: "mf" },
-                            (_, json) => {
-                                fs.writeFileSync(hebrewFilePath.replace(".po", ".json"), JSON.stringify(json, null, 4));
-                                console.log("Finished Hebrew translation compilation to JSON");
-                            });
-                    });
-            });
-        console.log("Updating translation signature");
-        fs.readFile(resourcesFilePath,
-            "utf8",
-            (err, data) => {
-                if (err) {
-                    return;
-                }
-                var result = data.replace(/(\.json\?sign=)(.*)"/g, "$1" + new Date().getTime().toString() + '"');
-                fs.writeFile(resourcesFilePath, result, "utf8", () => { });
-            });
+}
+
+downloadAndProcess("he", hebrewFilePath);
+downloadAndProcess("en-US", englishFilePath);
+
+console.log("Updating translation signature");
+fs.readFile(resourcesFilePath,
+    "utf8",
+    (err, data) => {
+        if (err) {
+            return;
+        }
+        const result = data.replace(/(\.json\?sign=)(.*)"/g, "$1" + new Date().getTime().toString() + '"');
+        fs.writeFile(resourcesFilePath, result, "utf8", () => { });
     });
 
