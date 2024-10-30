0746530282335a9c7c0eead0082f3fe9c4e3882d
HarelM <harel.mazor@gmail.com>
2024-10-30 23:38:42 +0200

    Improve coverage for geojson utils

--- IsraelHiking.Web/src/application/services/geojson-utils.spec.ts
+++ IsraelHiking.Web/src/application/services/geojson-utils.spec.ts
@@ -1,6 +1,24 @@
 import { GeoJSONUtils } from "./geojson-utils";
 
 describe("GeoJsonUtils", () => {
+    it("should set a property", () => {
+        const feature = {properties: {}} as any as GeoJSON.Feature;
+        GeoJSONUtils.setProperty(feature, "name", "name");
+        expect(feature.properties.name).toBe("name");
+    });
+
+    it("should set a property that already exists", () => {
+        const feature = {properties: {name: "name"}} as any as GeoJSON.Feature;
+        GeoJSONUtils.setProperty(feature, "name", "name1");
+        expect(feature.properties.name1).toBe("name1");
+    });
+
+    it("should set a property that already exists", () => {
+        const feature = {properties: {name: "name", name1: "name1"}} as any as GeoJSON.Feature;
+        GeoJSONUtils.setProperty(feature, "name", "name2");
+        expect(feature.properties.name2).toBe("name2");
+    });
+
     it("should get extenal description for hebrew", () => {
         const results = GeoJSONUtils.getExternalDescription(
             {properties: { "poiExternalDescription:he": "desc"}} as any as GeoJSON.Feature, "he");
@@ -38,5 +56,13 @@ describe("GeoJsonUtils", () => {
 
     it("should return has extra data for feature with image", () => {
         expect(GeoJSONUtils.hasExtraData({properties: { image: "image-url"}} as any as GeoJSON.Feature, "he")).toBeTruthy();
+    });
+
+    it("should return has extra data for feature with wikipedia", () => {
+        expect(GeoJSONUtils.hasExtraData({properties: { wikipedia: "wiki" }} as any as GeoJSON.Feature, "he")).toBeTruthy();
+    });
+
+    it("should return has extra data for feature with wikidat", () => {
+        expect(GeoJSONUtils.hasExtraData({properties: { wikidata: "wiki" }} as any as GeoJSON.Feature, "he")).toBeTruthy();
     });
 });
