00bbdca608c28409a0a1d712d71e7864ec9bf624
HarelM <harel.mazor@gmail.com>
2025-05-03 23:25:05 +0300

    Split code into methods

--- IsraelHiking.Web/scripts/white-label-me.ts
+++ IsraelHiking.Web/scripts/white-label-me.ts
@@ -1,6 +1,5 @@
 import { MobileProject } from '@trapezedev/project';
 import type { MobileProjectConfig } from '@trapezedev/project';
-import path from 'path';
 import fs from 'fs';
 
 const config: MobileProjectConfig = {
@@ -18,40 +17,48 @@ const version = '1.2.34';
 const buildNumber = 1234;
 const websiteUrl = 'www.somthing.com';
 
-// Update capcacitor config
-const configPath = 'capacitor.config.ts';
-let configFile = fs.readFileSync(configPath, 'utf8');
+function updateCapacitorConfigFile() {
+    const configPath = 'capacitor.config.ts';
+    let configFile = fs.readFileSync(configPath, 'utf8');
 
-// Example: replace appId and appName dynamically
-configFile = configFile
-  .replace(/appId: ['"`](.*?)['"`]/, `appId: '${appId}'`)
-  .replace(/appName: ['"`](.*?)['"`]/, `appName: '${appName}'`);
+    // Example: replace appId and appName dynamically
+    configFile = configFile
+    .replace(/appId: ['"`](.*?)['"`]/, `appId: '${appId}'`)
+    .replace(/appName: ['"`](.*?)['"`]/, `appName: '${appName}'`);
 
-fs.writeFileSync(configPath, configFile);
+    fs.writeFileSync(configPath, configFile);
+}
 
-
-const project = new MobileProject('.', config);
-await project.load();
-
-await project.android?.setPackageName(appId);
-await project.android?.setVersionName(version);
-await project.android?.setVersionCode(buildNumber);
-await project.android?.getResourceXmlFile("res/values/strings.xml")?.setAttrs("package_name", appId);
-await project.android?.getResourceXmlFile("res/values/strings.xml")?.setAttrs("plugin_bgloc_account_type", appId + "account");
-await project.android?.getResourceXmlFile("res/values/strings.xml")?.setAttrs("app_name", appName);
-const appBuildGradleFile = await project.android?.getGradleFile('app/build.gradle');
-await appBuildGradleFile?.setApplicationId(appId);
-await appBuildGradleFile?.setNamespace(appId);
-for (const intentFilter of project.android?.getAndroidManifest().find('manifest/application/activity/intent-filter/data')) {
-    if (intentFilter.getAttribute('android:host') === "israelhiking.osm.org.il") {
-        intentFilter.setAttribute('android:host', websiteUrl);
+async function updateAndroidFiles(project: MobileProject) {
+    await project.android?.setPackageName(appId);
+    await project.android?.setVersionName(version);
+    await project.android?.setVersionCode(buildNumber);
+    await project.android?.getResourceXmlFile("res/values/strings.xml")?.setAttrs("package_name", appId);
+    await project.android?.getResourceXmlFile("res/values/strings.xml")?.setAttrs("plugin_bgloc_account_type", appId + "account");
+    await project.android?.getResourceXmlFile("res/values/strings.xml")?.setAttrs("app_name", appName);
+    const appBuildGradleFile = await project.android?.getGradleFile('app/build.gradle');
+    await appBuildGradleFile?.setApplicationId(appId);
+    await appBuildGradleFile?.setNamespace(appId);
+    for (const intentFilter of project.android?.getAndroidManifest().find('manifest/application/activity/intent-filter/data')) {
+        if (intentFilter.getAttribute('android:host') === "israelhiking.osm.org.il") {
+            intentFilter.setAttribute('android:host', websiteUrl);
+        }
     }
 }
 
-await project.ios?.setBundleId(null, "Release", appId);
-await project.ios?.setDisplayName(null, "Release", appName);
-await project.ios?.setBuild(null, "Release", version);
-await project.ios?.setVersion(null, "Release", version);
+async function updateIosFiles(project: MobileProject) {
+    await project.ios?.setBundleId(null, "Release", appId);
+    await project.ios?.setDisplayName(null, "Release", appName);
+    await project.ios?.setBuild(null, "Release", version);
+    await project.ios?.setVersion(null, "Release", version);
+}
 
+
+///// Main function to run the script //////
+updateCapacitorConfigFile();
+const project = new MobileProject('.', config);
+await project.load();
+updateAndroidFiles(project);
+updateIosFiles(project);
 await project.commit();
 
