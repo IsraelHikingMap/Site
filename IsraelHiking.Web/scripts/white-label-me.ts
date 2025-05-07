5381a334bd8245081b9075b303a2008b1268d88c
HarelM <harel.mazor@gmail.com>
2025-05-07 23:56:02 +0300

    More fixes in paths

--- IsraelHiking.Web/scripts/white-label-me.ts
+++ IsraelHiking.Web/scripts/white-label-me.ts
@@ -36,12 +36,12 @@ async function searchAndReplaceInFiles() {
         from: new RegExp(oldAppName, 'g'),
         to: newAppName
     });
-    
+
     await replaceInFile({
         files: [
             "**/metadata/**/*.*",
             "**/*.html",
-            "../**/robot.txt",
+            "../**/robots.txt",
             "**/environments/environment.mobile.ts",
             "**/open-with.service.ts",
         ],
