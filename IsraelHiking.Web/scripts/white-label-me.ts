34bdafc6cfaeb2d592e9204e8eff4ac76d72eb2a
HarelM <harel.mazor@gmail.com>
2025-05-11 23:14:30 +0300

    Fix incorrect argv usage, Fix lint

--- IsraelHiking.Web/scripts/white-label-me.ts
+++ IsraelHiking.Web/scripts/white-label-me.ts
@@ -1,13 +1,13 @@
-import { MobileProject } from '@trapezedev/project';
-import { replaceInFile } from 'replace-in-file'
-import type { MobileProjectConfig } from '@trapezedev/project';
+import { MobileProject } from "@trapezedev/project";
+import { replaceInFile } from "replace-in-file"
+import type { MobileProjectConfig } from "@trapezedev/project";
 
 const config: MobileProjectConfig = {
     ios: {
-        path: 'ios/App',
+        path: "ios/App",
     },
     android: {
-        path: 'android',
+        path: "android",
     },
 };
 
@@ -18,21 +18,23 @@ if (process.argv.length < 3) {
     process.exit(1);
 }
 
-const version = process.argv[2] || '9.20.0';
+const version = process.argv[2] || "9.20.0";
 const buildNumber = +process.argv[3] || 920000;
 
 const oldAppName = "Israel Hiking Map";
-const oldAppId = 'il.org.osm.israelhiking';
-const oldWebsiteUrl = 'israelhiking.osm.org.il';
+const oldAppId = "il.org.osm.israelhiking";
+const oldWebsiteUrl = "israelhiking.osm.org.il";
 
-const newAppName = "Israel Hiking Map";
-const newAppId = 'il.org.osm.israelhiking';
-const newWebsiteUrl = 'israelhiking.osm.org.il';
+let newAppName = "Israel Hiking Map";
+let newAppId = "il.org.osm.israelhiking";
+let newWebsiteUrl = "israelhiking.osm.org.il";
+let supportEmail = "israelhiking@osm.rg.il";
 
-if (process.argv[3] === 'mapeak') {
-    const newAppName = "Mapeak";
-    const newAppId = 'com.mapeak.www';
-    const newWebsiteUrl = 'www.mapeak.com';
+if (process.argv[4] === "mapeak") {
+    newAppName = "Mapeak";
+    newAppId = "com.mapeak";
+    newWebsiteUrl = "www.mapeak.com";
+    supportEmail = "support@mapeak.com"
 }
 
 async function searchAndReplaceInFiles() {
@@ -48,7 +50,7 @@ async function searchAndReplaceInFiles() {
             "**/favicons/*.*",
             "**/*.service.ts"
         ],
-        from: new RegExp(oldAppName, 'g'),
+        from: new RegExp(oldAppName, "g"),
         to: newAppName
     });
 
@@ -61,7 +63,7 @@ async function searchAndReplaceInFiles() {
             "**/open-with.service.ts",
             "**/App.entitlements"
         ],
-        from: new RegExp(oldWebsiteUrl.replace(/\./g, "\\."), 'g'),
+        from: new RegExp(oldWebsiteUrl.replace(/\./g, "\\."), "g"),
         to: newWebsiteUrl
     });
 
@@ -73,7 +75,7 @@ async function searchAndReplaceInFiles() {
             "**/project.pbxproj",
             "../**/*.cs",
         ],
-        from: new RegExp(oldAppId, 'g'),
+        from: new RegExp(oldAppId, "g"),
         to: newAppId
     });
 }
@@ -85,24 +87,24 @@ async function updateAndroidFiles(project: MobileProject) {
     const stringsFile = await project.android?.getResourceXmlFile("values/strings.xml");
     await stringsFile.load();
     for (let element of stringsFile.find("resources/string")) {
-        if (element.getAttribute('name') === 'app_name') {
+        if (element.getAttribute("name") === "app_name") {
             element.textContent = newAppName;
         }
-        if (element.getAttribute('name') === 'plugin_bgloc_account_type') {
+        if (element.getAttribute("name") === "plugin_bgloc_account_type") {
             element.textContent = newAppId + ".account";
         }
-        if (element.getAttribute('name') === 'package_name') {
+        if (element.getAttribute("name") === "package_name") {
             element.textContent = newAppId;
         }
     }
     await project.android?.getResourceXmlFile("res/values/strings.xml")?.setAttrs("plugin_bgloc_account_type", newAppId + "account");
     await project.android?.getResourceXmlFile("res/values/strings.xml")?.setAttrs("app_name", newAppName);
-    const appBuildGradleFile = await project.android?.getGradleFile('app/build.gradle');
+    const appBuildGradleFile = await project.android?.getGradleFile("app/build.gradle");
     await appBuildGradleFile?.setApplicationId(newAppId);
     await appBuildGradleFile?.setNamespace(newAppId);
-    for (const intentFilter of project.android?.getAndroidManifest().find('manifest/application/activity/intent-filter/data')) {
-        if (intentFilter.getAttribute('android:host') === oldWebsiteUrl) {
-            intentFilter.setAttribute('android:host', newWebsiteUrl);
+    for (const intentFilter of project.android?.getAndroidManifest().find("manifest/application/activity/intent-filter/data")) {
+        if (intentFilter.getAttribute("android:host") === oldWebsiteUrl) {
+            intentFilter.setAttribute("android:host", newWebsiteUrl);
         }
     }
 }
@@ -118,7 +120,7 @@ async function updateIosFiles(project: MobileProject) {
 
 
 ///// Main function to run the script //////
-const project = new MobileProject('.', config);
+const project = new MobileProject(".", config);
 await project.load();
 await updateAndroidFiles(project);
 await updateIosFiles(project);
