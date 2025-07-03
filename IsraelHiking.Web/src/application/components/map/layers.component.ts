640947aabaf81d6f0baa79b15c2eaf4498abada6
HarelM <harel.mazor@gmail.com>
2025-07-03 12:50:35 +0300

    Use a small map to create an image for the share

---
+++ IsraelHiking.Web/src/application/components/map/layers.component.ts
@@ -0,0 +1,41 @@
+import { Component, inject } from "@angular/core";
+import { NgFor, AsyncPipe } from "@angular/common";
+import { Angulartics2OnModule } from "angulartics2";
+import { Observable } from "rxjs";
+import { Store } from "@ngxs/store";
+import type { Immutable } from "immer";
+
+import { AutomaticLayerPresentationComponent } from "./automatic-layer-presentation.component";
+import { LayersService } from "../../services/layers.service";
+import { ResourcesService } from "../../services/resources.service";
+import type { ApplicationState, Overlay } from "../../models/models";
+
+@Component({
+    selector: "layers",
+    templateUrl: "layers.component.html",
+    imports: [AutomaticLayerPresentationComponent, NgFor, Angulartics2OnModule, AsyncPipe]
+})
+export class LayersComponent {
+    public overlays$: Observable<Immutable<Overlay[]>>;	
+
+    public readonly resources = inject(ResourcesService);
+
+    private readonly layersService = inject(LayersService);
+    private readonly store = inject(Store);
+
+    constructor() {
+        this.overlays$ = this.store.select((state: ApplicationState) => state.layersState.overlays);
+    }
+
+    public getBaseLayer() {
+        return this.layersService.getSelectedBaseLayer();
+    }
+
+    public trackByKey(_: number, el: Overlay) {
+        return el.key;
+    }
+
+    public isSameBaselayerOn(overlay: Overlay) {
+        return overlay.address === this.getBaseLayer()?.address;
+    }
+}
