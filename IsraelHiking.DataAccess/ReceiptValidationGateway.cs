c07a3e1f868cdd5f7e03633f48161595e022c006
HarelM <harel.mazor@gmail.com>
2025-08-19 15:59:39 +0300

    Make entitelement check more robuts

--- IsraelHiking.DataAccess/ReceiptValidationGateway.cs
+++ IsraelHiking.DataAccess/ReceiptValidationGateway.cs
@@ -7,8 +7,30 @@ using Microsoft.Extensions.Options;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Linq;
 
 namespace IsraelHiking.DataAccess;
+
+public class IapticPurchaseResponse
+{
+    [JsonPropertyName("purchases")]
+    public Dictionary<string, IapticPurchase> Purchases { get; set; }
+}
+
+public class IapticPurchase
+{
+    [JsonPropertyName("isExpired")]
+    public bool IsExpired { get; set; }
+}
+
+public class RevenueCatEntitlementsResponse
+{
+    [JsonPropertyName("items")]
+    public List<object> Items { get; set; }
+}
 
 public class ReceiptValidationGateway(
     IHttpClientFactory httpClientFactory,
@@ -29,14 +51,15 @@ public class ReceiptValidationGateway(
         var responseStr = await response.Content.ReadAsStringAsync();
         if (response.StatusCode != HttpStatusCode.OK)
         {
-            throw new Exception("There was a problem communicating with the receipt validation server, code: " 
+            throw new Exception("There was a problem communicating with the receipt validation server, code: "
                                 + response.StatusCode + ", " + responseStr);
         }
-        var returnValue = responseStr.Contains("\"isExpired\":false");
-        logger.LogInformation("Is entitled with Iaptic for user: " + userId + " is: " + returnValue);
-        if (returnValue)
+        var iapticResponse = JsonSerializer.Deserialize<IapticPurchaseResponse>(responseStr);
+        var iapticEntitled = iapticResponse.Purchases.Values.Any(v => v.IsExpired == false);
+        logger.LogInformation("Is entitled with Iaptic for user: " + userId + " is: " + iapticEntitled);
+        if (iapticEntitled)
         {
-            return true;
+            //return true;
         }
         //https://api.revenuecat.com/v2/projects/proj1b16c0fa/customers/1257210/active_entitlements
         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _options.RevenueCatApiKey);
@@ -44,11 +67,11 @@ public class ReceiptValidationGateway(
         responseStr = await response.Content.ReadAsStringAsync();
         if (response.StatusCode != HttpStatusCode.OK)
         {
-            throw new Exception("There was a problem communicating with the receipt validation server, code: " 
+            throw new Exception("There was a problem communicating with the receipt validation server, code: "
                                 + response.StatusCode + ", " + responseStr);
         }
-        returnValue = responseStr.Contains("\"items\":[{\"");
-        logger.LogInformation("Is entitled with Revenuecat for user: " + userId + " is: " + returnValue);
-        return returnValue;
+        var hasEntitlements = JsonSerializer.Deserialize<RevenueCatEntitlementsResponse>(responseStr).Items.Any();
+        logger.LogInformation("Is entitled with Revenuecat for user: " + userId + " is: " + hasEntitlements);
+        return hasEntitlements;
     }
 }
