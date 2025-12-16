97201048a93bdb418703cdc4d72918a41d175449
zstadler <zeev.stadler@gmail.com>
2025-12-16 14:49:19 +0200

    Update Android app URL to remove language parameter (#2298)
    Removed the 'hl=en' query parameter from the Android app URL.

--- IsraelHiking.Web/src/application/urls.ts
+++ IsraelHiking.Web/src/application/urls.ts
@@ -41,6 +41,6 @@ export class Urls {
     public static readonly HEATMAP_TILES_ADDRESS =
         "https://raw.githubusercontent.com/IsraelHikingMap/VectorMap/master/Styles/OSM_traces.json";
 
-    public static readonly ANDROID_APP_URL = "https://play.google.com/store/apps/details?id=il.org.osm.israelhiking&hl=en";
+    public static readonly ANDROID_APP_URL = "https://play.google.com/store/apps/details?id=il.org.osm.israelhiking";
     public static readonly IOS_APP_URL = "https://apps.apple.com/us/app/mapeak/id6751947875";
 }
