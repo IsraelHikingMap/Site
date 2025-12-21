622cb47209076769f940aa63dcb1d80564d42333
HarelM <harel.mazor@gmail.com>
2025-12-21 15:42:38 +0200

    Redirect app on start, remove logo.

--- IsraelHiking.Web/src/application/app.routes.ts
+++ IsraelHiking.Web/src/application/app.routes.ts
@@ -4,9 +4,11 @@ import { MainMapComponent } from "./components/map/main-map.component";
 import { PrivacyPolicyComponent } from "./components/screens/privacy-policy.component";
 import { FaqComponent } from "./components/screens/faq.component";
 import { AttributionComponent } from "./components/screens/attribution.component";
+import { environment } from "environments/environment";
 
 export const routes: Route[] = [
-    { path: "", component: LandingComponent },
+    { path: "", redirectTo: environment.isCapacitor ? "/map" : "/about", pathMatch: "full" },
+    { path: "about", component: LandingComponent },
     { path: "privacy-policy", component: PrivacyPolicyComponent },
     { path: "faq", component: FaqComponent },
     { path: "attribution", component: AttributionComponent },
