79cda2f4876463caf0a80404e7eb356a22cf1a9e
HarelM <harel.mazor@gmail.com>
2026-01-08 14:34:57 +0200

    Add back app intro.

--- IsraelHiking.Web/src/application/services/application-initialize.service.ts
+++ IsraelHiking.Web/src/application/services/application-initialize.service.ts
@@ -4,6 +4,7 @@ import { Store } from "@ngxs/store";
 
 import { UseAppDialogComponent } from "../components/dialogs/use-app-dialog.component";
 import { FacebookWarningDialogComponent } from "../components/dialogs/facebook-warning-dialog.component";
+import { IntroDialogComponent } from "../components/dialogs/intro-dialog.component";
 import { LoggingService } from "./logging.service";
 import { ScreenService } from "./screen.service";
 import { DatabaseService } from "./database.service";
@@ -81,6 +82,9 @@ export class ApplicationInitializeService {
                 } else {
                     UseAppDialogComponent.openDialog(this.dialog);
                 }
+            } else if (this.runningContextService.isCapacitor
+                && this.store.selectSnapshot((s: ApplicationState) => s.configuration).isShowIntro) {
+                IntroDialogComponent.openDialog(this.dialog, this.runningContextService);
             }
             this.poiService.initialize(); // do not wait for it to complete
             this.recordedRouteService.initialize();
