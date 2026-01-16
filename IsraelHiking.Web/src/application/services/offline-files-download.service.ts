97e94417e2fbca30917b62243f555af90f88645b
HarelM <harel.mazor@gmail.com>
2026-01-16 17:23:37 +0200

    Add debug message

--- IsraelHiking.Web/src/application/services/offline-files-download.service.ts
+++ IsraelHiking.Web/src/application/services/offline-files-download.service.ts
@@ -140,8 +140,8 @@ export class OfflineFilesDownloadService {
             try {
                 const version = await this.pmtilesService.getVersion(fileNameAndDate.fileName);
                 metadata.push({ fileName: fileNameAndDate.fileName, date: fileNameAndDate.date, version });
-            } catch {
-                // ignore this
+            } catch (ex) {
+                this.loggingService.error(`[Offline Download] Failed to get version for file: ${fileNameAndDate.fileName}, error: ${ex}`);
             }
         }
         return metadata;
