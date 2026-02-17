0d6722a3f56dda197e14b8e99761095750ee5d56
Harel M <harel.mazor@gmail.com>
2026-02-17 21:40:39 +0200

    Move to capgo inappbrowser (#2371)
    * Migrate to capgo oauth instead of cordova
    
    * Use local version of capgo with the relevant changes
    
    * Update to use latest version.
    
    * Update to use the published version
    
    * Update configuration.
    
    * cap sync
    
    * Move to local version of inapbrowser
    
    * Update to latest version
    
    * Remove dead code
    
    * Simplify code, reduce PR changes
    
    * Remove patch-package, add linux dependencies.
    
    * Remove unused script

--- IsraelHiking.Web/src/application/services/authorization.service.ts
+++ IsraelHiking.Web/src/application/services/authorization.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { firstValueFrom } from "rxjs";
 import { Store } from "@ngxs/store";
+import { InAppBrowser } from "@capgo/inappbrowser";
 
 import { RunningContextService } from "./running-context.service";
 import { LoggingService } from "./logging.service";
@@ -35,23 +36,26 @@ export class AuthorizationService {
             return;
         }
         this.loggingService.info("[Authorization] User initiated login");
-        this.logout();
-        // this has to be here in order to support safari on desktop since it can only open a window on click event
-        const popup = this.openWindow();
         const params = new URLSearchParams({
             client_id: "jqxu2hhG-gUa-XUxiepzkQPZQf7iQguMC0sTVSRpaKE",
             redirect_uri: this.redirectUrl,
             response_type: "code",
             scope: "read_prefs write_api read_gpx write_gpx"
         });
-        const oauthCode = await this.getCodeFromWindow(popup, Urls.osmAuth + "/authorize?" + params.toString());
-        const accessToken = await this.getAccessToken(oauthCode);
+        const result = await InAppBrowser.openSecureWindow({
+            authEndpoint: Urls.osmAuth + "/authorize?" + params.toString(),
+            redirectUri: this.redirectUrl,
+            broadcastChannelName: "osm-api-auth-complete"
+        });
+        const redirectedUrl = new URL(result.redirectedUri);
+        const code = redirectedUrl.searchParams.get(AuthorizationService.OAUTH_CODE);
+        const accessToken = await this.getAccessToken(code);
         this.store.dispatch(new SetTokenAction(accessToken));
         await this.updateUserDetails();
     }
 
     private async getAccessToken(oauthCode: string): Promise<string> {
-        const accessTokenUrl =  Urls.osmAuth + "/token";
+        const accessTokenUrl = Urls.osmAuth + "/token";
         const response = await firstValueFrom(this.httpClient.post<{ access_token: string }>(accessTokenUrl, null, {
             params: {
                 client_id: "jqxu2hhG-gUa-XUxiepzkQPZQf7iQguMC0sTVSRpaKE",
@@ -59,7 +63,7 @@ export class AuthorizationService {
                 code: oauthCode,
                 redirect_uri: this.redirectUrl,
             },
-            headers: {"Content-Type": "application/x-www-form-urlencoded" }
+            headers: { "Content-Type": "application/x-www-form-urlencoded" }
         }));
         return response.access_token;
     }
@@ -76,51 +80,4 @@ export class AuthorizationService {
         this.loggingService.info(`[Authorization] User ${userInfo.displayName} logged-in successfully`);
     };
 
-    private openWindow(): Window {
-        if (this.runningContextService.isCapacitor) {
-            return null;
-        }
-        // Create a 600x550 popup window in the center of the screen
-        const w = 600;
-        const h = 550;
-        const settings = [
-            ["width", w], ["height", h],
-            ["left", screen.width / 2 - w / 2],
-            ["top", screen.height / 2 - h / 2]
-        ].map((x) => x.join("=")).join(",");
-
-        return window.open("about:blank", "Authorization", settings);
-    }
-
-    private getCodeFromWindow(popup: Window, authorizeUrl: string): Promise<string> {
-        return new Promise((resolve, reject) => {
-            if (!this.runningContextService.isCapacitor) {
-                if (typeof popup.focus === "function") {
-                    popup.focus();
-                }
-                popup.location.href = authorizeUrl;
-                const bc = new BroadcastChannel("osm-api-auth-complete");
-                bc.addEventListener("message", (event) => {
-                    const redirectedUrl = new URL(event.data);
-                    bc.close();
-                    resolve(redirectedUrl.searchParams.get(AuthorizationService.OAUTH_CODE));
-                });
-                setTimeout(() => {
-                    bc.close();
-                    reject(new Error("The OSM sign in flow timed out"))
-                }, 5*60000);
-            } else {
-                const callback = (event: MessageEvent) => {
-                    if (event.data.match(/^oauth::/)) {
-                        const data = JSON.parse(event.data.substring(7));
-                        window.removeEventListener("message", callback);
-                        resolve(data[AuthorizationService.OAUTH_CODE]);
-                    }
-                };
-                window.addEventListener("message", callback);
-
-                window.open(authorizeUrl, "oauth:osm", "");
-            }
-        });
-    }
 }
