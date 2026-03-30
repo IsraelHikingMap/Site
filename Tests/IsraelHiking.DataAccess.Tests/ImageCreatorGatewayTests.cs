b447f5facb1eca6861868a28252e71b21672556a
Harel M <harel.mazor@gmail.com>
2026-03-30 14:39:55 +0300

    Transition to tileserver gl for recording creation (#2432)
    * Change gateway to use the new service.
    
    * Change configuration, remove from docker compose.
    
    * Remove console writeline

---
+++ Tests/IsraelHiking.DataAccess.Tests/ImageCreatorGatewayTests.cs
@@ -0,0 +1,48 @@
+using IsraelHiking.Common;
+using IsraelHiking.Common.Configuration;
+using IsraelHiking.Common.DataContainer;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.IO;
+using System.Net.Http;
+
+namespace IsraelHiking.DataAccess.Tests;
+
+[TestClass]
+public class ImageCreatorGatewayTests
+{
+    [TestMethod]
+    [Ignore]
+    public void CreateImage()
+    {
+        var factory = Substitute.For<IHttpClientFactory>();
+        factory.CreateClient().Returns(new HttpClient());
+        var options = Substitute.For<IOptions<ConfigurationData>>();
+        options.Value.Returns(new ConfigurationData());
+        var gateway = new ImageCreationGateway(factory, options);
+        var results = gateway.Create(new DataContainerPoco
+        {
+            Routes = [
+                new RouteData {
+                    Segments = [
+                        new RouteSegmentData {
+                            Latlngs = [
+                                new LatLngTime {
+                                    Lat = 30,
+                                    Lng = 35
+                                },
+                                new LatLngTime {
+                                    Lat = 30.001,
+                                    Lng = 35
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }, 128, 128).Result;
+        Assert.IsTrue(results.Length > 0);
+        File.WriteAllBytes("/Users/harelmazor/dev/Site/IsraelHiking.Web/temp.jpg", results);
+    }
+}
