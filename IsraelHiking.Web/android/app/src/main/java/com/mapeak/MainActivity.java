27743a994aba7e21837549111c763d06d8a6e508
HarelM <harel.mazor@gmail.com>
2026-04-03 12:10:02 +0300

    When search is focused almost all the screen is hidden #2435 - workaround Android 9 issues with keyboard gap.

--- IsraelHiking.Web/android/app/src/main/java/com/mapeak/MainActivity.java
+++ IsraelHiking.Web/android/app/src/main/java/com/mapeak/MainActivity.java
@@ -1,5 +1,17 @@
 package com.mapeak;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.view.WindowManager;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) { // Android 9 and below
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
+        }
+    }
+}
