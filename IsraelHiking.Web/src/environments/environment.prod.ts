144dfd2a318c5ca8898191f93c9928b07569ac71
HarelM <harel.mazor@gmail.com>
2026-04-26 00:23:19 +0300

    Fix production build

--- IsraelHiking.Web/src/environments/environment.prod.ts
+++ IsraelHiking.Web/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    baseAddress: window.location.origin,
-    baseApiAddress: window.location.origin + "/api/",
-    baseTilesAddress: window.location.origin,
+    baseAddress: typeof window !== "undefined" ? window.location.origin : "https://mapeak.com",
+    baseApiAddress: typeof window !== "undefined" ? window.location.origin + "/api/" : "https://mapeak.com/api",
+    baseTilesAddress: typeof window !== "undefined" ? window.location.origin : "https://mapeak.com",
     isCapacitor: false
 };
