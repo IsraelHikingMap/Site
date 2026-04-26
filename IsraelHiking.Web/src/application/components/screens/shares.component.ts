89d643f70e60397a7918777eaae2d0b1a6924738
HarelM <harel.mazor@gmail.com>
2026-04-26 22:26:49 +0300

    Revert unneeded change

--- IsraelHiking.Web/src/application/components/screens/shares.component.ts
+++ IsraelHiking.Web/src/application/components/screens/shares.component.ts
@@ -167,8 +167,7 @@ export class SharesComponent implements OnInit {
         this.routesGeoJson = { type: "FeatureCollection", features };
         this.showMap = true;
         const bounds = SpatialService.getBoundsForFeatureCollection(this.routesGeoJson);
-        const bottom = typeof window !== "undefined" ? window.innerHeight / 2 : 0;
-        this.mapService.fitBounds(bounds, 100, { top: 150, left: 50, bottom, right: 50 });
+        this.mapService.fitBounds(bounds, 100, { top: 150, left: 50, bottom: window.innerHeight / 2, right: 50 });
     }
 
     public getIconFromType(shareUrl: Immutable<ShareUrl>) {
