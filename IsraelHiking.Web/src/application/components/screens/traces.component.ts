12931b2f1c7183046fbb0c20cc6a1ac9c0b94937
HarelM <harel.mazor@gmail.com>
2026-04-26 22:27:39 +0300

    Revert unneeded change

--- IsraelHiking.Web/src/application/components/screens/traces.component.ts
+++ IsraelHiking.Web/src/application/components/screens/traces.component.ts
@@ -292,8 +292,7 @@ export class TracesComponent implements OnInit {
         this.selectedTraceGeoJson = { type: "FeatureCollection", features };
         this.showMap = true;
         const bounds = SpatialService.getBoundsForFeatureCollection(this.selectedTraceGeoJson);
-        const bottom = typeof window !== "undefined" ? window.innerHeight / 3 : 0;
-        this.mapService.fitBounds(bounds, 100, { top: 100, left: 50, bottom, right: 50 });
+        this.mapService.fitBounds(bounds, 100, { top: 100, left: 50, bottom: window.innerHeight / 3, right: 50 });
     }
 
     public onStartPointClick(traceId: string, event?: Event) {
