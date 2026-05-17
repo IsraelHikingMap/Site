43f0d80b989498c669d057a6426bbe07a19c42b2
HarelM <harel.mazor@gmail.com>
2026-05-17 12:16:15 +0300

    Reduce changes.

--- IsraelHiking.Web/src/application/services/image-resize.service.spec.ts
+++ IsraelHiking.Web/src/application/services/image-resize.service.spec.ts
@@ -22,109 +22,64 @@ describe("ImageResizeService", () => {
         });
     });
 
-    it("Should fial to convert the image without location data", inject(
-        [ImageResizeService],
-        async (service: ImageResizeService) => {
-            await expect(
-                service.resizeImageAndConvert(new Blob([""]) as File)
-            ).rejects.toThrow();
-        }
-    ));
+    it("Should fial to convert the image without location data", inject([ImageResizeService], async (service: ImageResizeService) => {
+        await expect(service.resizeImageAndConvert(new Blob([""]) as File)).rejects.toThrow();
+    }));
 
-    it("Should not fail when 0th is missing", inject(
-        [ImageResizeService],
-        async (service: ImageResizeService) => {
-            const exifData: IExif = {};
-            const exifbytes = dump(exifData);
-            const dataUrl = insert(exifbytes, IMAGE_BASE_64);
-            const blob = dataUrlToBlob(dataUrl);
-            await expect(service.resizeImage(blob as File)).resolves.not.toThrow();
-        }
-    ));
+    it("Should not fail when 0th is missing", inject([ImageResizeService], async (service: ImageResizeService) => {
+        const exifData: IExif = {};
+        const exifbytes = dump(exifData);
+        const dataUrl = insert(exifbytes, IMAGE_BASE_64);
+        const blob = dataUrlToBlob(dataUrl);
+        await expect(service.resizeImage(blob as File)).resolves.not.toThrow();
+    }));
 
-    it("Should not fail when GPS values are empty", inject(
-        [ImageResizeService],
-        async (service: ImageResizeService) => {
-            const exifData: IExif = {
-                GPS: {
-                    [TagValues.GPSIFD.GPSLatitude]: [
-                        [0, 1],
-                        [0, 1],
-                        [0, 1],
-                    ],
-                    [TagValues.GPSIFD.GPSLongitude]: [
-                        [0, 1],
-                        [0, 1],
-                        [0, 1],
-                    ],
-                    [TagValues.GPSIFD.GPSLatitudeRef]: "",
-                    [TagValues.GPSIFD.GPSLongitudeRef]: "",
-                },
-            };
-            const exifbytes = dump(exifData);
-            const dataUrl = insert(exifbytes, IMAGE_BASE_64);
-            const blob = dataUrlToBlob(dataUrl);
-            await expect(
-                service.resizeImageAndConvert(blob as File)
-            ).rejects.toThrowError("Image does not contain geolocation information");
-        }
-    ));
+    it("Should not fail when GPS values are empty", inject([ImageResizeService], async (service: ImageResizeService) => {
+        const exifData: IExif = {
+            GPS: {
+                [TagValues.GPSIFD.GPSLatitude]: [[0, 1], [0, 1], [0, 1]],
+                [TagValues.GPSIFD.GPSLongitude]: [[0, 1], [0, 1], [0, 1]],
+                [TagValues.GPSIFD.GPSLatitudeRef]: "",
+                [TagValues.GPSIFD.GPSLongitudeRef]: "",
+            },
+        };
+        const exifbytes = dump(exifData);
+        const dataUrl = insert(exifbytes, IMAGE_BASE_64);
+        const blob = dataUrlToBlob(dataUrl);
+        await expect(service.resizeImageAndConvert(blob as File)).rejects.toThrow("Image does not contain geolocation information");
+    }));
 
-    it("Should not fail when GPS values are bad", inject(
-        [ImageResizeService],
-        async (service: ImageResizeService) => {
-            const exifData: IExif = {
-                GPS: {
-                    [TagValues.GPSIFD.GPSLatitude]: [
-                        [0, 0],
-                        [0, 1],
-                        [0, 1],
-                    ],
-                    [TagValues.GPSIFD.GPSLongitude]: [
-                        [0, 0],
-                        [0, 1],
-                        [0, 1],
-                    ],
-                    [TagValues.GPSIFD.GPSLatitudeRef]: "S",
-                    [TagValues.GPSIFD.GPSLongitudeRef]: "W",
-                },
-            };
-            const exifbytes = dump(exifData);
-            const dataUrl = insert(exifbytes, IMAGE_BASE_64);
-            const blob = dataUrlToBlob(dataUrl);
-            await expect(
-                service.resizeImageAndConvert(blob as File)
-            ).rejects.toThrowError("Image does not contain geolocation information");
-        }
-    ));
+    it("Should not fail when GPS values are bad", inject([ImageResizeService], async (service: ImageResizeService) => {
+        const exifData: IExif = {
+            GPS: {
+                [TagValues.GPSIFD.GPSLatitude]: [[0, 0], [0, 1], [0, 1]],
+                [TagValues.GPSIFD.GPSLongitude]: [[0, 0], [0, 1], [0, 1]],
+                [TagValues.GPSIFD.GPSLatitudeRef]: "S",
+                [TagValues.GPSIFD.GPSLongitudeRef]: "W",
+            },
+        };
+        const exifbytes = dump(exifData);
+        const dataUrl = insert(exifbytes, IMAGE_BASE_64);
+        const blob = dataUrlToBlob(dataUrl);
+        await expect(service.resizeImageAndConvert(blob as File)).rejects.toThrow("Image does not contain geolocation information");
+    }));
 
-    it("Should convert the image with location data", inject(
-        [ImageResizeService],
-        async (service: ImageResizeService) => {
-            const exifData: IExif = {
-                GPS: {
-                    [TagValues.GPSIFD.GPSLatitude]: [
-                        [2, 1],
-                        [0, 1],
-                        [0, 1],
-                    ],
-                    [TagValues.GPSIFD.GPSLongitude]: [
-                        [3, 1],
-                        [0, 1],
-                        [0, 1],
-                    ],
-                    [TagValues.GPSIFD.GPSLatitudeRef]: "S",
-                    [TagValues.GPSIFD.GPSLongitudeRef]: "W",
-                },
-            };
-            const exifbytes = dump(exifData);
-            const dataUrl = insert(exifbytes, IMAGE_BASE_64);
-            const blob = dataUrlToBlob(dataUrl);
-            const dataContainer = await service.resizeImageAndConvert(blob as File);
-            expect(dataContainer.routes.length).toBe(1);
-            expect(dataContainer.routes[0].markers.length).toBe(1);
-            expect(dataContainer.routes[0].markers[0].latlng.lat).toBe(-2);
-            expect(dataContainer.routes[0].markers[0].latlng.lng).toBe(-3);
-        }
-    ));
+    it("Should convert the image with location data", inject([ImageResizeService], async (service: ImageResizeService) => {
+        const exifData: IExif = {
+            GPS: {
+                [TagValues.GPSIFD.GPSLatitude]: [[2, 1], [0, 1], [0, 1]],
+                [TagValues.GPSIFD.GPSLongitude]: [[3, 1], [0, 1], [0, 1]],
+                [TagValues.GPSIFD.GPSLatitudeRef]: "S",
+                [TagValues.GPSIFD.GPSLongitudeRef]: "W",
+            },
+        };
+        const exifbytes = dump(exifData);
+        const dataUrl = insert(exifbytes, IMAGE_BASE_64);
+        const blob = dataUrlToBlob(dataUrl);
+        const dataContainer = await service.resizeImageAndConvert(blob as File);
+        expect(dataContainer.routes.length).toBe(1);
+        expect(dataContainer.routes[0].markers.length).toBe(1);
+        expect(dataContainer.routes[0].markers[0].latlng.lat).toBe(-2);
+        expect(dataContainer.routes[0].markers[0].latlng.lng).toBe(-3);
+    }));
 });
