4d92abaf1061d11d04f31be0dc08e65313985dee
HarelM <harel.mazor@gmail.com>
2026-05-17 12:47:44 +0300

    Add coverage to client side.

--- IsraelHiking.Web/vitest.config.ts
+++ IsraelHiking.Web/vitest.config.ts
@@ -3,16 +3,16 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [
-    nodePolyfills({
-      include: ["stream", "timers", "events", "buffer", "string_decoder"],
-      exclude: ["module"],
-    }),
-  ],
-  resolve: {
-    alias: {
-      fflate: resolve(__dirname, "node_modules/fflate/esm/browser.js"),
-      "piexif-ts": resolve(__dirname, "node_modules/piexif-ts/dist/piexif.js"),
-    },
-  },
+    plugins: [
+        nodePolyfills({
+            include: ["stream", "timers", "events", "buffer", "string_decoder"],
+            exclude: ["module"],
+        }),
+    ],
+    resolve: {
+        alias: {
+            fflate: resolve(__dirname, "node_modules/fflate/esm/browser.js"),
+            "piexif-ts": resolve(__dirname, "node_modules/piexif-ts/dist/piexif.js"),
+        },
+    }
 });
